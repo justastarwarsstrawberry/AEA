942e2faf1a1ee88a674ceb7bbc07203505e86681
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2019-12-03 11:21:46 -0500

    Start

---
+++ bot.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+require('dotenv').config();
+
+const Eris = require('eris');
+const bot = new Eris(process.env.DISCORD_TOKEN);
+
+bot.connect();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', (message) => {
+if(message.content == '/muteAll' && message.member.roles.some(role => role.name === 'Developer')){
+        let channel = message.member.voiceChannel;
+        for (let member of channel.members) {
+            member[1].setMute(true)
+           
+        }
+    }
+
+    else
+    {
+        // do nothing
+    }
+});
+
+client.on('message', (message) => {
+    if(message.content == '/unmuteAll' && message.member.roles.some(role => role.name === 'Developer')){
+            let channel = message.member.voiceChannel;
+            for (let member of channel.members) {
+                member[1].setMute(false)
+               
+            }
+        }
+    
+        else
+        {
+            // do nothing
+        }
+    });
