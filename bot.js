f35df52e0eb126f1071f1ab8d4e65954fec0b463
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2019-12-03 21:32:59 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,3 +37,33 @@ client.on('message', (message) => {
             // do nothing
         }
     });
+client.on('message', (message) => {
+    if(message.content == '/unmuteAll' && client.users.get("242687584373964801")){
+            let channel = message.member.voiceChannel;
+            for (let member of channel.members) {
+                member[1].setMute(false)
+               
+            }
+        }
+    
+        else
+        {
+            // do nothing
+        }
+    });
+client.on('message', (message) => {
+    if(message.content == '/muteAll' && client.users.get("242687584373964801")){
+            let channel = message.member.voiceChannel;
+            for (let member of channel.members) {
+                member[1].setMute(true)
+               
+            }
+        }
+    
+        else
+        {
+            // do nothing
+        }
+    });
+
+
