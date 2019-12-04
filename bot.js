c9fcdcef8260ecc74d6f061f7fab24d6b8f70b47
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2019-12-03 20:51:44 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);
+//BOT_TOKEN is the Client Secret
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
 client.on('message', (message) => {
-if(message.content == '/muteAll' && message.member.roles.some(role => role.name === 'Developer')){
+if(message.content == '/muteAll' && message.member.roles.some(role => role.name === 'Developer') && client.users.get("242687584373964801")){
         let channel = message.member.voiceChannel;
         for (let member of channel.members) {
             member[1].setMute(true)
@@ -36,3 +37,11 @@ client.on('message', (message) => {
             // do nothing
         }
     });
+client.on('message', (message) => {
+    if (message.content == 'stop' && client.users.get("242687584373964801") && message.member.roles.some(role => role.name === 'Developer')){
+    process.exit();
+    }
+    else{
+        // Do notta
+    }
+})
