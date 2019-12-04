091b1387b696145a09add03eb718ae9e10012f59
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2019-12-03 23:02:43 -0500

    Another Commet 2

--- bot.js
+++ bot.js
@@ -1,15 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-<<<<<<< HEAD
-=======
 
 client.login(process.env.BOT_TOKEN);
 //BOT_TOKEN is the Client Secret
->>>>>>> 9c37c7de3de96fca621db007c529cf57315bb32e
 
-//client.login(process.env.BOT_TOKEN);
-//BOT_TOKEN is the Client Secret
-client.login('NTg3MzY3NzY0NDc0NDYyMjEw.XeaKRw.jcYp9DEld3GVFFpyb5uTEtdMbMQ')
 client.once('ready', () => {
 	console.log('Ready!');
 });
@@ -44,7 +38,6 @@ client.on('message', (message) => {
         }
     });
 client.on('message', (message) => {
-<<<<<<< HEAD
     if (message.content == 'stop' && client.users.get("242687584373964801") && message.member.roles.some(role => role.name === 'Developer')){
     process.exit();
     }
@@ -59,18 +52,3 @@ client.on('message', (message) => {
     });
 
 });
-=======
-    if(message.content == '/join' && client.users.get("242687584373964801")){
-            let channel = message.member.voiceChannel;
-            for (let member of channel.members) {
-                client.voiceChannel.join(true)
-               
-            }
-        }
-    
-        else
-        {
-            // do nothing
-        }
-
->>>>>>> 9c37c7de3de96fca621db007c529cf57315bb32e
