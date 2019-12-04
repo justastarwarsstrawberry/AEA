5bd0d581e784481d79af3c9958fcc2b58680d2e3
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2019-12-04 12:08:58 -0500

    Another Commet

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const { Client, Attachment } = require('discord.js');
 
 client.login(process.env.BOT_TOKEN);
 //BOT_TOKEN is the Client Secret
@@ -51,4 +52,31 @@ client.on('message', (message) => {
      const dispatcher = connection.playFile('C:/Users/Lemon/OneDrive/Documents/Music/Desktop/discord bots/Austins/resources/bruh.ogg');
     });
 
+});
+client.on('message', (message) => {
+    if (message.content.startsWith('/kick') && client.users.get("242687584373964801")) {
+    const user = message.mentions.users.first();
+    if (user){
+    const member = message.guild.member(user);
+    if (member){
+    member.kick('Kicked by Austin').then(() => {
+    // We let the message author know we were able to kick the person
+    message.reply(`Successfully kicked ${user.tag}`);
+                });
+            }
+        }
+    }
+});
+client.on('message', message => {
+    const attachment = new Attachment('./resources/NuclearDrone.png');
+    if (message.content === 'bruh') {
+
+        message.channel.send(attachment);
+        
+    }
+});
+client.on('message', message => {
+    if (client.users.get('587367764474462210') || message.content === './resources/NuclearDrone.png'){
+        message.delete(2000);
+    }
 });
