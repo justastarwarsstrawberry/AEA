39f1be8a7e3d56920f5f36a3e7a372f66cdeb67c
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2019-12-05 15:17:44 -0500

    And Another One

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { Client, Attachment } = require('discord.js');
+client.setMaxListeners(0)
 
 client.login(process.env.BOT_TOKEN);
 //BOT_TOKEN is the Client Secret
