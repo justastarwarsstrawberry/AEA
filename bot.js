f51cf77655b65cba4469a89b612e3853fbc04d34
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2019-12-09 11:49:44 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -742,7 +742,7 @@ client.on('message', message => {
     if (message.content === 'heavy missile ship' ||  message.content === 'heavymissileship' || message.content === 'Heavy Missile Ship' || message.content === 'HeavyMissileShip' || message.content === 'heavy missile ship' || message.content === 'heavymissileship' || message.content === 'Heavy Missile S' || message.content === 'HeavyMissileS') {
         const attachment = new Attachment('./resources/hvymissileship.png');
 
-        if (message.content === 'heavy missile ship' ||  message.content === 'heavymissileship' || message.content === 'Heavy Missle Ship' || message.content === 'HeavyMissileShip' || message.content === 'heavy missile ship' || message.content === 'heavymissileship' || message.content === 'Heavy Missile S' || message.content === 'HeavyMissileS') {
+        if (message.content === 'heavy missile ship' ||  message.content === 'heavymissileship' || message.content === 'Heavy Missile Ship' || message.content === 'HeavyMissileShip' || message.content === 'heavy missile ship' || message.content === 'heavymissileship' || message.content === 'Heavy Missile S' || message.content === 'HeavyMissileS') {
         message.channel.send(attachment);
         }
         message.reply(`
