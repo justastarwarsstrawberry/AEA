f32fe6ebd0aec1182409678ae01b11870ad2e6fb
Daniel2201 <58594890+Daniel2201@users.noreply.github.com>
2019-12-10 21:34:42 +0200

    Update anubis.js

--- coders/anubis.js
+++ coders/anubis.js
@@ -38,12 +38,14 @@ client.on('message', message => {
 
     }
 });
-   function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-   } 
-       client.on('message', message => {
+ 
+  
+         client.on('message', message => {
+             function getRandomInt(min, max) {
+                  min = Math.ceil(min);
+                  max = Math.floor(max);
+                 return Math.floor(Math.random() * (max - min)) + min;
+             }
            if(message.content === '/random') {
            var x = getRandomInt(1, 36)
            if (x===1) {
@@ -55,7 +57,7 @@ client.on('message', message => {
                message.channel.send(attachment);
            }
             if (x===3) {
-               const attachment = new Attachment('./resources/[p4]Desert Battle (4p)_map.png');
+               const attachment = new Attachment('./resources/[p4]Desert Battle (4p)_map.PNG');
                message.channel.send(attachment);
            }
              if (x===4) {
@@ -68,16 +70,138 @@ client.on('message', message => {
            
            }
               if (x===6) {
-               const attachment = new Attachment('./resources/[p5]Eye of Magma (2v3)_map.png');
+               const attachment = new Attachment('./resources/[p5]Eye of Magma (2v3)_map.PNG');
                message.channel.send(attachment);
            
            }
               if (x===7) {
-               const attachment = new Attachment('./resources/[p5]Eye of Magma (2v3)_map.png');
+               const attachment = new Attachment('./resources/[p5]Hourglass (2v3)_map.png');
                message.channel.send(attachment);
            
        }
+              if (x===8) {
+               const attachment = new Attachment('./resources/[p5]Kratos and Hercules (2v3)_map.png');
+               message.channel.send(attachment);
            
+       }
+              if (x===9) {
+               const attachment = new Attachment('./resources/[p5]Kratos and Hercules (2v3)_map.png');
+               message.channel.send(attachment);
+           
+       }
+              if (x===10) {
+               const attachment = new Attachment('./resources/[p6] RoundaBout (6p)_map.PNG');
+               message.channel.send(attachment);
+           
+       }
+              if (x===11) {
+               const attachment = new Attachment('./resources/[p6]Beach Landing mrhidihoEdit (6p)_map.png');
+               message.channel.send(attachment);
+           
+       }
+              if (x===12) {
+               const attachment = new Attachment('./resources/[p6]Beach Landing mrhidihoEdit (6p)_map.png');
+               message.channel.send(attachment);
+           
+       }
+              if (x===13) {
+               const attachment = new Attachment('./resources/[p6]Bridge of Hell (6p)_map.png');
+               message.channel.send(attachment);
+           
+       }
+               
+              if (x===14) {
+               const attachment = new Attachment('./resources/[p6]Burning Beaches (6p)_map.png');
+               message.channel.send(attachment);
+           
+       }
+               if (x===15) {
+               const attachment = new Attachment('./resources/[p6]Crossing (6p) mrhidihoEdit_map.png');
+               message.channel.send(attachment);
+           
+       }
+               if (x===16) {
+               const attachment = new Attachment('./resources/[p6]Desert Crossing (6p)_map.PNG');
+               message.channel.send(attachment);
+           
+       }
+               if (x===17) {
+               const attachment = new Attachment('./resources/[p6]Divided Islands (6p)_map.png');
+               message.channel.send(attachment);
+           
+       }
+               if (x===18) {
+               const attachment = new Attachment('./resources/[p6]Igneous-small (6p)_map.png');
+               message.channel.send(attachment);
+           
+       }
+               if (x===19) {
+               const attachment = new Attachment('./resources/[p6]Magma Island mrhidihoEdit (6p)_map.png');
+               message.channel.send(attachment);
+           
+       }
+               if (x===20) {
+               const attachment = new Attachment('./resources/[p6]Serpent's Pass (6p)_map.png');
+               message.channel.send(attachment);
+           
+       }
+               if (x===21) {
+               const attachment = new Attachment('./resources/[p6]Tundra (6p)_map.png');
+               message.channel.send(attachment);
+           
+       }
+               if (x===22) {
+               const attachment = new Attachment('./resources/[p6]Weddell Isles (6p)_map.png');
+               message.channel.send(attachment);
+           
+       }
+               if (x===23) {
+               const attachment = new Attachment('./resources/[p6]X-Path (6p)_map.png');
+               message.channel.send(attachment);
+           
+       }
+               if (x===24) {
+               const attachment = new Attachment('./resources/[p8]Darklord-Egypt (8p)_map.png');
+               message.channel.send(attachment);
+           
+       }
+               if (x===25) {
+               const attachment = new Attachment('./resources/[p8]Igneous (8p)_map.png');
+               message.channel.send(attachment);
+           
+       }
+               if (x===26) {
+               const attachment = new Attachment('./resources/[p8]Many Islands mrhidihoEdit (8p)_map.PNG');
+               message.channel.send(attachment);
+           
+       }
+               if (x===27) {
+               const attachment = new Attachment('./resources/[p9]Coliding Powers (4v5)_map.png');
+               message.channel.send(attachment);
+           
+       }
+               if (x===28) {
+               const attachment = new Attachment('./resources/[p9]Kings of the Hills (9p)_map.png');
+               message.channel.send(attachment);
+           
+       }
+               if (x===29) {
+               const attachment = new Attachment('./resources/[p9]No-Mans Valley (3v4)_map.png');
+               message.channel.send(attachment);
+           
+       }
+               if (x===30) {
+               const attachment = new Attachment('./resources/[p10] Napali Warfare (10p)_map.PNG');
+               message.channel.send(attachment);
+           
+       }
+               if (x===31) {
+               const attachment = new Attachment('./resources/[p10] Ultimate Crossing (10p)_map.png');
+               message.channel.send(attachment);
+           
+       }
+});
+});
 });
     
 //Dont touch Zone start
