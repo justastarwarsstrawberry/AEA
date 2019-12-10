6f2c32cb8743083108f2b5fdf7d62bcfcb06f966
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2019-12-10 12:36:32 -0500

    Update anubis.js

--- coders/anubis.js
+++ coders/anubis.js
@@ -1,6 +1,7 @@
 //Dont touch Zone start
 exports.code = function(){
 const { Client, Attachment } = require('discord.js');
+client.setMaxListeners(0)
 //Dont touch Zone end
 
 client.on('message', message => {
