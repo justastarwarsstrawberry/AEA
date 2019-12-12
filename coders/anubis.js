12b9b4d499aa34dc817daba19472ee903af8c581
Daniel2201 <58594890+Daniel2201@users.noreply.github.com>
2019-12-12 19:55:00 +0200

    Update anubis.js

--- coders/anubis.js
+++ coders/anubis.js
@@ -277,6 +277,21 @@ client.on('message', message => {
        }
     }
 });
+    client.on('message', message => {
+    if (message.content === '/flip a coin) {
+       function getRandomInt(min, max) {
+                  min = Math.ceil(min);
+                  max = Math.floor(max);
+                 return Math.floor(Math.random() * (max - min)) + min;
+       var flip = getRandomInt(1, 2);
+        if (flip === '1') {
+        message.reply('Tails')
+                } else {
+        message.reply('Heads')
+            }
+            }
+        }
+    }
     
 //Dont touch Zone start
 }
