f1c40545addb0115c86d76d0ccbce67dd6f37046
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2019-12-12 12:19:36 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,18 +33,13 @@ client.on('guildMemberAdd', member => {
 });
 const talkedRecently = new Set();
 const commands = new RichEmbed()
-	.setColor('#747575')
+	.setColor('#1500f7')
 	.setTitle('Commands')
 	.setURL('')
 	.setAuthor('SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128')
-	.setDescription('Commands for AEA')
 	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
-	.addField('Regular field title', 'Some value here')
+	.addField('/random', '2v2', '3v3', '4v4')
 	.addBlankField()
-	.addField('Inline field title', 'Some value here', true)
-	.addField('Inline field title', 'Some value here', true)
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
 	.setTimestamp()
 	.setFooter('Thats all folks!', 'https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128');
 
