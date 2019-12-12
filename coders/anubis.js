2eae7d96dbde71a3d3e4e0e868c0197cfb564c27
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2019-12-12 13:18:34 -0500

    Update anubis.js

--- coders/anubis.js
+++ coders/anubis.js
@@ -285,7 +285,7 @@ client.on('message', message => {
         if (flip === '1') {
         message.reply('Tails')
             } 
-        if (flip === '2'{
+        if (flip === '2'){
         message.reply('Heads')
             }
         }
