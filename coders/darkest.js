ecb304647f7eb762e6af44b790270b81e2cc06aa
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2019-12-18 11:37:37 -0500

    Update darkest.js

--- coders/darkest.js
+++ coders/darkest.js
@@ -3,7 +3,19 @@ exports.code = function(){
 const { Client, Attachment } = require('discord.js');
 client.setMaxListeners(0)
 //Dont touch Zone end
-  client.on(
+  
+  
+client.on('message', message => {
+//units code
+    if (message.content === 'Unitex' ||  message.content === 'Exunit' || message.content === 'unitex') {
+	    message.channel.send(unitex);
+    }
+  
+});
+  
+  
+  
+  
 //Dont touch Zone start
 }
 //Dont touch Zone end
