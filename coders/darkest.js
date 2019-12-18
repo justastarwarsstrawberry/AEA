32f957e10c9abde9062b7b9745265d121c613fc0
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2019-12-18 11:36:02 -0500

    Update darkest.js

--- coders/darkest.js
+++ coders/darkest.js
@@ -3,3 +3,7 @@ exports.code = function(){
 const { Client, Attachment } = require('discord.js');
 client.setMaxListeners(0)
 //Dont touch Zone end
+  client.on(
+//Dont touch Zone start
+}
+//Dont touch Zone end
