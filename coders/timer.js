79c526039600a0278c0262077340beb1d9cf44df
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2019-12-19 11:21:50 -0500

    Update timer.js

--- coders/timer.js
+++ coders/timer.js
@@ -16,23 +16,23 @@ const Timer2m = setTimeout(timeup, 80000);
 const Timer3m = setTimeout(timeup, 120000);
 const Timer10m = setTimerout(timeup, 600000);
 if(message.content == '/startTimer'){
-  if(message.content.includes('10s') && message.content == '/startTimer'){
+  if(message.content.includes('10s')){
   message.reply('Timer started for 10 seconds')
   Timer10s;
   }
-  if(message.content.includes('1m') && message.content == '/startTimer'){
+  if(message.content.includes('1m')){
   message.reply('Timer Started for 1 minute')
   Timer1m;
   }
-  if(message.content.includes('2m') && message.content == '/startTimer'){
+  if(message.content.includes('2m')){
   message.reply('Timer Started for 2 minutes')
   Timer2m;
   }
-  if(message.content.includes('3m') && message.content == '/startTimer'){
+  if(message.content.includes('3m')){
   message.reply('Timer Started for 3 minutes')
   Timer3m;
   }
-  if(message.content.includes('10m') && message.content == '/startTimer'){
+  if(message.content.includes('10m')){
   message.reply('Timer Started for 10 minutes')
   Timer10m;
   }
