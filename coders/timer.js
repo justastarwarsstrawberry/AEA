3f5364fff58fb539e308c7cb3c984d48287866c9
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2019-12-19 11:16:09 -0500

    Update timer.js

--- coders/timer.js
+++ coders/timer.js
@@ -1,17 +1,63 @@
+//_____________Dont touch Zone start
+exports.code = function(){
+const { Client, Attachment } = require('discord.js');
+client.setMaxListeners(0)
+
+  
 client.on("message", message => {
 
 //timer future implementation
-const Timer
-if(message.content == 'Timer'){
-
+function timeup() {
+  message.reply('Times Up!')
 }
-
-  /**
-   * Clears a timeout.
-   * @param {Timeout} timeout Timeout to cancel
-   */
-if(message.content == '/stoptimer'){
-
+const Timer10s = setTimeout(timeup, 10000);
+const Timer1m = setTimeout(timeup, 60000);
+const Timer2m = setTimeout(timeup, 80000);
+const Timer3m = setTimeout(timeup, 120000);
+const Timer10m = setTimerout(timeup, 600000);
+if(message.content == '/startTimer'){
+  if(message.content.includes('10s' && message.content == '/startTimer'){
+  message.reply('Timer started for 10 seconds)
+  Timer10s;
+  }
+  if(message.content.includes('1m') && message.content == '/startTimer'){
+  message.reply('Timer Started for 1 minute')
+  Timer1m;
+  }
+  if(message.content.includes('2m') && message.content == '/startTimer'){
+  message.reply('Timer Started for 2 minutes')
+  Timer2m;
+  }
+  if(message.content.includes('3m') && message.content == '/startTimer'){
+  message.reply('Timer Started for 3 minutes')
+  Timer3m;
+  }
+  if(message.content.includes('10m') && message.content == '/startTimer'){
+  message.reply('Timer Started for 10 minutes')
+  Timer10m;
+  }
+}
+if(message.content == '/stopTimer'){
+  message.reply('Stopped Timer')
+  if(Timer10s) {
+  clearTimeout(Timer10s, 250);
+  }
+  if(Timer1m) {
+  clearTimeout(Timer1m, 250);
+  }
+  if(Timer2m) {
+  clearTimeout(Timer2m, 250);
+  }
+  if(Timer3m) {
+  clearTimeout(Timer3m, 250);
+  }
+  if(Timer10m) {
+  clearTimeout(Timer10m, 250);
+  }
 }
 
 });
+  
+
+}
+//________________Dont touch Zone end
