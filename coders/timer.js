0f7cec4e411aaafdfbbce88e724856b986212312
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2019-12-19 11:35:45 -0500

    Update timer.js

--- coders/timer.js
+++ coders/timer.js
@@ -9,52 +9,52 @@ client.on("message", message => {
 //timer future implementation
 function timeup() {
   message.reply('Times Up!')
+  clearTimeout(Timer10s, 250);
+  clearTimeout(Timer1m, 250);
+  clearTimeout(Timer2m, 250);
+  clearTimeout(Timer3m, 250);
+  clearTimeout(Timer10m, 250);
 }
-const Timer10s = setTimeout(timeup, 10000);
-const Timer1m = setTimeout(timeup, 60000);
-const Timer2m = setTimeout(timeup, 80000);
-const Timer3m = setTimeout(timeup, 120000);
-const Timer10m = setTimeout(timeup, 600000);
+
+
+
+
 
   if(message.content === '10s'){
+  var Timer10s = setTimeout(timeup, 10000);
   message.reply('Timer started for 10 seconds')
   Timer10s;
   }
   if(message.content === '1m'){
+  var Timer1m = setTimeout(timeup, 60000);
   message.reply('Timer Started for 1 minute')
   Timer1m;
   }
   if(message.content === '2m'){
+  var Timer2m = setTimeout(timeup, 80000);
   message.reply('Timer Started for 2 minutes')
   Timer2m;
   }
   if(message.content === '3m'){
+  var Timer3m = setTimeout(timeup, 120000);
   message.reply('Timer Started for 3 minutes')
   Timer3m;
   }
   if(message.content === '10m'){
+  var Timer10m = setTimeout(timeup, 600000);
   message.reply('Timer Started for 10 minutes')
   Timer10m;
   }
-
+}
 if(message.content == '/stopTimer'){
   message.reply('Stopped Timer')
-  if(Timer10s) {
   clearTimeout(Timer10s, 250);
-  }
-  if(Timer1m) {
   clearTimeout(Timer1m, 250);
-  }
-  if(Timer2m) {
   clearTimeout(Timer2m, 250);
-  }
-  if(Timer3m) {
   clearTimeout(Timer3m, 250);
-  }
-  if(Timer10m) {
   clearTimeout(Timer10m, 250);
   }
-}
+
 
 });
   
