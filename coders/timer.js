553ddbb996af8dd7be9722ea8e3b312aa6654156
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2019-12-19 10:48:04 -0500

    Create timer.js

---
+++ coders/timer.js
@@ -0,0 +1,17 @@
+client.on("message", message => {
+
+//timer future implementation
+const Timer
+if(message.content == 'Timer'){
+
+}
+
+  /**
+   * Clears a timeout.
+   * @param {Timeout} timeout Timeout to cancel
+   */
+if(message.content == '/stoptimer'){
+
+}
+
+});
