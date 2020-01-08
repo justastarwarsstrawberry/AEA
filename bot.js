2ba9335181747177dee6cc640dcb341ab46196f2
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-01-08 10:30:40 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,7 +61,7 @@ const clist = new RichEmbed()
 	.addField('/flipcoin', 'Heads or Tails?', false)
 	.addField('/list', 'Land, Air, Water, Exp', false)
 	.addField('<unit>', 'name of the unit', false)
-	.addField('<time>', 'amount of time max: 20m', false)
+	.addField('<time>', 'amount of time max: 10m', false)
 	.setDescription('Lists all comands \nassociated with the bot')
 	.setTimestamp()
 	.setFooter('More Soon!', 'https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128');
