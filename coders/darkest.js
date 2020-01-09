035564e24678b65a146e89d20927abc7cd852631
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-01-09 09:07:42 -0500

    Update darkest.js

--- coders/darkest.js
+++ coders/darkest.js
@@ -13,30 +13,198 @@ client.setMaxListeners(0)
 // Units Description 
 // Note: The description has to be outside client.on({});
 
-const unitex = new Discord.RichEmbed()
+const tank = new Discord.RichEmbed()
 	.setColor('#1500f7')
+	.setTitle('Tank')
 	.setURL('')
-	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
-	.attachFiles(['./resources/lynx.png'])
-	.setImage('attachment://lynx.png')
-
-	.setAuthor('DarkestHero', 'https://cdn.discordapp.com/avatars/356130618020593667/1d470fea1df7589c7ede6c906deec660.png?size=1024')
-
-	.setTitle('unitex')
-	.setDescription(" - Very fast \n- Low armor \n- Ground attack")
+	.attachFiles(['./resources/tank.png'])
+	.setAuthor('LemonsHQ', 'https://cdn.discordapp.com/avatars/242687584373964801/4b20eb44bfff0b11f7447ed582fedbb2.png?size=128')
+	.setDescription(" - Can attack ground only \n- Fast but low health")
 	.addBlankField()
-	.addField("Stats", "Price $275 \n Health 120 \n Attack Range 130 \n Move Speed 1.60 \n Build Speed 3.3s")
-
+	.addField("Stats", "Price $350 \n Health 230 \n Attack Range 130 \n Move Speed 1.10 \n Build Speed 4.8s")
+	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
+	.setImage('attachment://tank.png')
 	.setTimestamp()
 	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
-
+const missiletank = new Discord.RichEmbed()
+	.setColor('#1500f7')
+	.setTitle('AA Missile Tank')
+	.setURL('')
+	.attachFiles(['./resources/aamissiletank.png'])
+	.setAuthor('LemonsHQ', 'https://cdn.discordapp.com/avatars/242687584373964801/4b20eb44bfff0b11f7447ed582fedbb2.png?size=128')
+	.setDescription(" - Long range anti-air only missile \n- Light Shield")
+	.addBlankField()
+	.addField("Stats", "Price $2,500 \n Health 1,100 \n Attack Range 340 \n Shield 700 \n Move Speed 0.6 \n Build Speed 24.8s")
+	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
+	.setImage('attachment://aamissiletank.png')
+	.setTimestamp()
+	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
+const typhoon = new Discord.RichEmbed()
+	.setColor('#1500f7')
+	.setTitle('Typhoon')
+	.setURL('')
+	.attachFiles(['./resources/typhoon.png'])
+	.setAuthor('LemonsHQ', 'https://cdn.discordapp.com/avatars/242687584373964801/4b20eb44bfff0b11f7447ed582fedbb2.png?size=128')
+	.setDescription(" - Landing assault craft \n- Fast \n- Very light Mounted machine-guns \n- Carries 4 units")
+	.addBlankField()
+	.addField("Stats", "Price $2,500 \n Health 300 \n Attack Range 140 \n Move Speed 1.6 \n Build Speed 33.3s")
+	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
+	.setImage('attachment://typhoon.png')
+	.setTimestamp()
+	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
+const zephyr = new Discord.RichEmbed()
+	.setColor('#1500f7')
+	.setTitle('Zephyr')
+	.setURL('')
+	.attachFiles(['./resources/z.png'])
+	.setAuthor('LemonsHQ', 'https://cdn.discordapp.com/avatars/242687584373964801/4b20eb44bfff0b11f7447ed582fedbb2.png?size=128')
+	.setDescription(" - Medium \n- Armed with a prototype flamethrower \n- Short range ground attack \n- Can auto-repair")
+	.addBlankField()
+	.addField("Stats", "Price $1,750 \n Health 900 \n Attack Range 140 \n Move Speed 1.0 \n Build Speed 19.6s")
+	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
+	.setImage('attachment://z.png')
+	.setTimestamp()
+	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
+const plasmasniper = new Discord.RichEmbed()
+	.setColor('#1500f7')
+	.setTitle('Plasma Sniper')
+	.setURL('')
+	.attachFiles(['./resources/plasmasniper.png'])
+	.setAuthor('LemonsHQ', 'https://cdn.discordapp.com/avatars/242687584373964801/4b20eb44bfff0b11f7447ed582fedbb2.png?size=128')
+	.setDescription(" - Long range ground attack \n- Very weak armour with light shield \n- Can only hit ground")
+	.addBlankField()
+	.addField("Stats", "Price $1,600 \n Health 100 \n Shield 300 \n Attack Range 300 \n Move Speed 0.8 \n Build Speed 18.5s")
+	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
+	.setImage('attachment://plasmasniper.png')
+	.setTimestamp()
+	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
+const comet = new Discord.RichEmbed()
+	.setColor('#1500f7')
+	.setTitle('Comet')
+	.setURL('')
+	.attachFiles(['./resources/comet.png'])
+	.setAuthor('LemonsHQ', 'https://cdn.discordapp.com/avatars/242687584373964801/4b20eb44bfff0b11f7447ed582fedbb2.png?size=128')
+	.setDescription(" - Fast \n- Can attack ground only \n- Light armor \n- High spread, weak vs. single targets")
+	.addBlankField()
+	.addField("Stats", "Price $950 \n Health 450 \n Attack Range 150 \n Move Speed 1 \n Build Speed 11.1s")
+	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
+	.setImage('attachment://comet.png')
+	.setTimestamp()
+	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
+const houndapc = new Discord.RichEmbed()
+	.setColor('#1500f7')
+	.setTitle('Hound APC')
+	.setURL('')
+	.attachFiles(['./resources/houndapc.png'])
+	.setAuthor('LemonsHQ', 'https://cdn.discordapp.com/avatars/242687584373964801/4b20eb44bfff0b11f7447ed582fedbb2.png?size=128')
+	.setDescription(" - Long Range Attack \n- Powerful in groups, but glass cannons \n- Can only attack ground")
+	.addBlankField()
+	.addField("Stats", "Price $350 \n Health 100 \n Attack Range 130 \n Move Speed 1.4 \n Build Speed 3.3s")
+	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
+	.setImage('attachment://houndapc.png')
+	.setTimestamp()
+	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
+const heavytank = new Discord.RichEmbed()
+	.setColor('#1500f7')
+	.setTitle('Heavy Tank')
+	.setURL('')
+	.attachFiles(['./resources/hvytank.png'])
+	.setAuthor('LemonsHQ', 'https://cdn.discordapp.com/avatars/242687584373964801/4b20eb44bfff0b11f7447ed582fedbb2.png?size=128')
+	.setDescription(" - Heavily armed \n- Can attack ground \n- Light air attack")
+	.addBlankField()
+	.addField("Stats", "Price $800 \n Health 600 \n Attack Range 160 \n Move Speed 0.8 \n Build Speed 10.7s")
+	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
+	.setImage('attachment://hvytank.png')
+	.setTimestamp()
+	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
+const lasertank = new Discord.RichEmbed()
+	.setColor('#1500f7')
+	.setTitle('Laser Tank')
+	.setURL('')
+	.attachFiles(['./resources/lasertank.png'])
+	.setAuthor('LemonsHQ', 'https://cdn.discordapp.com/avatars/242687584373964801/4b20eb44bfff0b11f7447ed582fedbb2.png?size=128')
+	.setDescription(" - Can attack ground and air \n- Weak vs. multiple units \n- Powerful single shot, slow recharge")
+	.addBlankField()
+	.addField("Stats", "Price $1,300 \n Health 400 \n Attack Range 220 \n Move Speed 0.7 \n Build Speed 12.8s")
+	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
+	.setImage('attachment://lasertank.png')
+	.setTimestamp()
+	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
+const heavyartillery = new Discord.RichEmbed()
+	.setColor('#1500f7')
+	.setTitle('Heavy Artillery')
+	.setURL('')
+	.attachFiles(['./resources/hvyartillery.png'])
+	.setAuthor('LemonsHQ', 'https://cdn.discordapp.com/avatars/242687584373964801/4b20eb44bfff0b11f7447ed582fedbb2.png?size=128')
+	.setDescription(" - Can attack ground only \n- Long range \n- Limited Laser defence")
+	.addBlankField()
+	.addField("Stats", "Price $3,200 \n Health 600 \n Attack Range 310 \n Move Speed 0.7 \n Build Speed 24.8s")
+	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
+	.setImage('attachment://hvyartillery.png')
+	.setTimestamp()
+	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
+const DemoTruck = new Discord.RichEmbed()
+	.setColor('#1500f7')
+	.setTitle('Demo Truck')
+	.setURL('')
+	.attachFiles(['./resources/demotruck.png'])
+	.setAuthor('LemonsHQ', 'https://cdn.discordapp.com/avatars/242687584373964801/4b20eb44bfff0b11f7447ed582fedbb2.png?size=128')
+	.setDescription(" - Blows itself up on enemies \n- High damage and area of effect \n- Lower damage if killed before detonation \n- Light armor \n- Fast, but accelerates slowly \n- Cannot enter transports")
+	.addBlankField()
+	.addField("Stats", "Price $2,500 \n Health 420 \n Attack Range 9 \n Move Speed 1.5 \n Build Speed 41.7s")
+	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
+	.setImage('attachment://demotruck.png')
+	.setTimestamp()
+	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
   
 client.on('message', message => {
 //units code
-    if (message.content === 'Unitex' ||  message.content === 'Exunit' || message.content === 'unitex') {
-	    message.channel.send(unitex);
+    if (message.content === 'heavy tank' ||  message.content === 'heavytank' || message.content === 'Heavy Tank' || message.content === 'HeavyTank') {
+	message.channel.send(heavytank);
     }
-  
+
+    if (message.content === 'tank' ||  message.content === 'Tank') {
+	message.channel.send(tank);
+    }
+	
+    if (message.content === 'laser tank' ||  message.content === 'lasertank' || message.content === 'Laser Tank' || message.content === 'LaserTank') {
+	message.channel.send(lasertank);
+    }  
+
+    if (message.content === 'Hound Apc' ||  message.content === 'hound apc' || message.content === 'Hound APC') {
+	message.channel.send(houndapc);
+    }
+	
+    if (message.content === 'Comet' ||  message.content === 'comet') {
+	message.channel.send(comet);
+    }
+	
+    if (message.content === 'Heavy Artillery' ||  message.content === 'heavy artillery' || message.content === 'HeavyArtillery' || message.content === 'heavyartillery') {
+	message.channel.send(heavyartillery);
+    }
+	
+    if (message.content === 'Missile Tank' ||  message.content === 'missile tank' || message.content === 'MissileTank' || message.content === 'missiletank') {
+	message.channel.send(missiletank);
+    }
+	
+    if (message.content === 'Plasma Sniper' ||  message.content === 'plasma sniper' || message.content === 'PlasmaSniper' || message.content === 'plasmasniper') {
+        message.channel.send(plasmasniper);
+    }
+	
+    if (message.content === 'Zephyr' ||  message.content === 'zephyr') {
+        message.channel.send(zephyr);
+    }
+
+    if (message.content === 'demo truck' ||  message.content === 'Demo truck' || message.content === 'demoTruck' || message.content === 'demo Truck') {
+	message.channel.send(DemoTruck);
+    }
+
+
+    if (message.content === 'Typhon' ||  message.content === 'Typhoon' || message.content === 'typhoon') {
+	message.channel.send(typhoon);
+    }
+	
+	
 });
   
   
