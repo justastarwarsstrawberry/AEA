6f7486000e6d707482ef51bb53e363f64682fcaf
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-01-30 11:45:22 -0500

    Create ranks.js

---
+++ coders/ranks.js
@@ -0,0 +1,14 @@
+//_____________Dont touch Zone start
+exports.code = function(){
+const { Client, Attachment } = require('discord.js');
+client.setMaxListeners(0)
+//_____________Dont touch Zone end
+
+
+
+
+
+
+
+
+}
