b468c263a5177006715a19aec4a0423365f04f85
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-01-30 12:33:11 -0500

    Update ranks.js

--- coders/ranks.js
+++ coders/ranks.js
@@ -4,8 +4,13 @@ const { Client, Attachment } = require('discord.js');
 client.setMaxListeners(0)
 //_____________Dont touch Zone end
 
-
-
+  
+ client.on('message', (message, user) => {
+  
+   If(message.content.startsWith('/') && user.message.mentions(user)){
+     
+   }
+ });
 
 
 
