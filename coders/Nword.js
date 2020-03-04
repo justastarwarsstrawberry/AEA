101b619f6bf844ac585d5043225ccf2a3666660d
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-03-04 16:08:34 -0500

    Update Nword.js

--- coders/Nword.js
+++ coders/Nword.js
@@ -5,7 +5,7 @@ client.setMaxListeners(0)
 //_____________Dont touch Zone end
 
   
-const nword = new Discord.RichEmbed()
+const nworde = new Discord.RichEmbed()
 	.setColor('#1500f7')
 	.setTitle('The N Word has been spoken')
 	.attachFiles(['./resources/nwordmechs.jpg'])
@@ -16,19 +16,17 @@ const nword = new Discord.RichEmbed()
 	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
 
  client.on('message', (message, user) => {
-if (message.content.startsWith('n') || message.content.startsWith('N')) { 
-    if (message.content.endsWith('a') || message.content.endsWith('r') || message.content.endsWith('A') || message.content.endsWith('R')) {
-		if(message.content.includes('i') || message.content.includes('g') || message.content.includes('I') || message.content.includes('G')){
-	
-	message.delete(); 
-	const attachment = new Attachment('./resources/nwordmechs.jpg');
-	      message.reply('');
-              message.channel.send(nword);
-             
-    }
-  }
+let nword = ['nigger','nigga','nibba','nibber']
+let found = false;
+if (var i in nword){
+ if (message.content.toLowerCase().includes(nword[i].toLowerCase())) found = true;
 }
    
+if (found){
+ message.reply();
+ message.delete();
+ message.send(nworde);
+}
  });
 
 
