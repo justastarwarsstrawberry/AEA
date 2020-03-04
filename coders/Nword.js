2ab5b58acde3fe3116c8776f4777b9483ba26a69
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-03-04 12:12:57 -0500

    Update Nword.js

--- coders/Nword.js
+++ coders/Nword.js
@@ -5,17 +5,29 @@ client.setMaxListeners(0)
 //_____________Dont touch Zone end
 
   
+const nword = new Discord.RichEmbed()
+	.setColor('#1500f7')
+	.setTitle('The N Word has been spoken')
+	.attachFiles(['./resources/nwordmechs.jpg'])
+	.setDescription("The N Word has been spokenk, \nMay the rath of god banish them...")
+	.addBlankField()
+	.setImage('attachment://nwordmechs.jpg')
+	.setTimestamp()
+	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
+
  client.on('message', (message, user) => {
   
-	if (message.content.startswith('n') || message.content.startswith('N')) { 
+if (message.content.startswith('n') || message.content.startswith('N')) { 
     if (message.content.endswith('a') || message.content.endswith('r') || message.content.endswith('A') || message.content.endswith('R')) {
-		
-		message.delete(); 
-		 const attachment = new Attachment('./resources/nwordmechs.jpg');
-               message.channel.send(attachment);
-               message.reply('is racist');
-	}
- }
+		if(message.content.includes('i') || message.content.includes('g') || message.content.includes('I') || message.content.includes('G'){
+	message.delete(); 
+	const attachment = new Attachment('./resources/nwordmechs.jpg');
+	      message.reply('');
+              message.channel.send(nword);
+             
+    }
+  }
+}
    
  });
 
