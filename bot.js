c7b4b86ec996430d6e07d7282ea428efb5dffb5d
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-03-10 20:05:34 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -144,9 +144,9 @@ const titan = new Discord.RichEmbed()
 	.addField("Stats", "Price $250,000 \n Health 12,000 \n Shield 8,000 \n Attack Range 350 \n Move Speed 0.4 \n Build Speed Varies")
 	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
 	.setTimestamp()
-	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
 	.setImage('attachment://titan.png')
-
+	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
+	
 const expspider = new Discord.RichEmbed()
 	.setColor('#1500f7')
 	.setTitle('Experimental Spider')
