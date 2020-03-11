68feb7eef6126ab60f4c39b649e9dfe6f4ce0c68
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-03-10 20:08:46 -0400

    Update skaarjlord.js

--- coders/skaarjlord.js
+++ coders/skaarjlord.js
@@ -1,1 +1,9 @@
-
+//_____________Dont touch Zone start
+exports.code = function(){
+const { Client, Attachment } = require('discord.js');
+client.setMaxListeners(0)
+//_____________Dont touch Zone end
+  
+  
+  
+}
