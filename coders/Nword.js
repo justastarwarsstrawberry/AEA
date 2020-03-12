6f68e9ba96f4e2e8007ba687309095757db7fc79
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-03-12 09:41:44 -0400

    Update Nword.js

--- coders/Nword.js
+++ coders/Nword.js
@@ -16,22 +16,7 @@ const nworde = new Discord.RichEmbed()
 	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
 
 client.on('message', (message, user) => {
-let nword = ['nigger','nigga','nibba','nibber','nigg','nibb']
 
-const swearWords = ["darn", "shucks", "frak", "shite"];
-if( swearWords.some(word => message.content.includes(word)) ) {
-  message.reply("Oh no you said a bad word!!!");
-  // Or just do message.delete();
-}
-for(var i in nword){
-if(message.content.toLowerCase().includes(nword[i].toLowerCase())) found = true;
-}
-   
-if (found){
- message.reply();
- message.delete();
- message.channel.send(nworde);
-}
  });
 
 
