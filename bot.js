4ec29218eb847486bf49d53c089fb3b75f6bcbc1
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-03-12 23:04:13 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -112,7 +112,7 @@ const mlist = new RichEmbed()
 	.setURL('')
 	.setAuthor('SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128')
 	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
-	.addField('Mech Units', '', false)
+	.addField('Mech Units', 'Mechs Mechs and more', false)
 	.setDescription('Lists all mech units')
 	.setTimestamp()
 	.setFooter('More Soon!', 'https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128');
