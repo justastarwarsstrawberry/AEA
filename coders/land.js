dca0576d177f7d3931d15fd1c6e115d4223d07e2
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-03-13 10:15:07 -0400

    Update land.js

--- coders/land.js
+++ coders/land.js
@@ -252,6 +252,32 @@ const e18 = new Discord.RichEmbed()
 	.setImage('attachment://duster.png')
 	.setTimestamp()
 	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
+const e19 = new Discord.RichEmbed()
+	.setColor('#1500f7')
+	.setTitle('Artillery')
+	.setURL('')
+	.attachFiles(['./resources/artillery.png'])
+	.setAuthor('LemonsHQ', 'https://cdn.discordapp.com/avatars/242687584373964801/4b20eb44bfff0b11f7447ed582fedbb2.png?size=128')
+	.setDescription(" - Can attack ground only \n- Long range")
+	.addBlankField()
+	.addField("Stats", " Price $900 \n Health 140 \n Attack Range 290 \n Move Speed 0.90 \n Build Speed 8.3s")
+	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
+	.setImage('attachment://artillery.png')
+	.setTimestamp()
+	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
+const e20 = new Discord.RichEmbed()
+	.setColor('#1500f7')
+	.setTitle('Combat Engineer')
+	.setURL('')
+	.attachFiles(['./resources/cengineer.png'])
+	.setAuthor('LemonsHQ', 'https://cdn.discordapp.com/avatars/242687584373964801/4b20eb44bfff0b11f7447ed582fedbb2.png?size=128')
+	.setDescription(" - Can build units and some T2 buildings \n- Can attack land, air and subs \n- Can repair units \n- Can build Sub-turrets \n- Can only have 12 built at once")
+	.addBlankField()
+	.addField("Stats", " Price $7400 \n Health 1500 \n Attack Range 200 \n Move Speed 1.20 \n Build Speed 33.3s \n Income generation +$1")
+	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
+	.setImage('attachment://cengineer.png')
+	.setTimestamp()
+	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
 
 client.on('message', message => {
 	
@@ -327,6 +353,14 @@ client.on('message', message => {
 //duster
     if (message.content.toLowerCase() === db.P) {
 	message.channel.send(e18);
+    }	
+//artillery
+    if (message.content.toLowerCase() === db.T) {
+	message.channel.send(e19);
+    }	
+//combat engineer
+    if (message.content.toLowerCase() === db.T) {
+	message.channel.send(e20);
     }	
 });
   
