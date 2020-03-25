5297d7d42dc1ee0bd04e6d61705556a693ae718b
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2020-03-25 12:42:37 -0400

    Added two units to navy

--- coders/sea.js
+++ coders/sea.js
@@ -15,7 +15,33 @@ client.setMaxListeners(0)
 	.setAuthor('LemonsHQ', 'https://cdn.discordapp.com/avatars/242687584373964801/4b20eb44bfff0b11f7447ed582fedbb2.png?size=128')
 	.setDescription(" - Fast \n - Can attack ground")
 	.addBlankField()
-	.addField("Stats", " Price $300 \n Health 170 \n Attack Range 120 \n Move Speed 1.5 \n Build Speed 33.3s \n InIClass c_gunboat")
+	.addField("Stats", " Price $300 \n Health 170 \n Attack Range 120 \n Move Speed 1.5 \n Build Speed 3.3s \n InIClass c_gunboat")
+	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
+	.setImage('attachment://gunboat.jpg')
+	.setTimestamp()
+	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
+ const e1 = new Discord.RichEmbed()
+	.setColor('#1500f7')
+	.setTitle('Underwater Probe')
+	.setURL('')
+	.attachFiles(['./resources/underwaterprobe.png'])
+	.setAuthor('LemonsHQ', 'https://cdn.discordapp.com/avatars/242687584373964801/4b20eb44bfff0b11f7447ed582fedbb2.png?size=128')
+	.setDescription(" - Very weak \n - Light torpedo attack only")
+	.addBlankField()
+	.addField("Stats", " Price $450 \n Health 80 \n Attack Range 100 \n Move Speed 0.70 \n Build Speed 3.3s \n InIClass lightSub")
+	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
+	.setImage('attachment://underwaterprobe.png')
+	.setTimestamp()
+	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
+ const e2 = new Discord.RichEmbed()
+	.setColor('#1500f7')
+	.setTitle('Missile Ship')
+	.setURL('')
+	.attachFiles(['./resources/underwaterprobe.jpg'])
+	.setAuthor('LemonsHQ', 'https://cdn.discordapp.com/avatars/242687584373964801/4b20eb44bfff0b11f7447ed582fedbb2.png?size=128')
+	.setDescription(" - Fast \n - Can attack ground and air and subs \n - Weak amour")
+	.addBlankField()
+	.addField("Stats", " Price $800 \n Health 450 \n Attack Range 200 \n Move Speed 1.2 \n Build Speed 6.7s \n InIClass c_missileShip")
 	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
 	.setImage('attachment://gunboat.jpg')
 	.setTimestamp()
@@ -28,7 +54,12 @@ client.on('message', message => {
 if (message.content.toLowerCase() === db.A) {
 	message.channel.send(e0);
 }
-  
+if (message.content.toLowerCase() === db.B) {
+	message.channel.send(e1);
+} 
+if (message.content.toLowerCase() === db.C) {
+	message.channel.send(e2);
+}
 });
   
   
