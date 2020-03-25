1a1002e6c209dbcea6b843e114ce71bf80e57232
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2020-03-25 18:15:50 -0400

    Testing scary new function

--- coders/skaarjlord.js
+++ coders/skaarjlord.js
@@ -4,7 +4,26 @@ const { Client, Attachment } = require('discord.js');
 client.setMaxListeners(0)
 //_____________Dont touch Zone end
   
-client.on('message', message => {
+module.exports.run = async (bot, message, args) => {
   
-});
+  if(!message.member.roles.find(r => r.name === "Admin") || !message.member.roles.find(r => r.name === "Developer") || !message.member.roles.find(r => r.name === "Bot Developer")) return message.channel.send("Thoust are not able")
+  let argsresult;
+  message.delete()
+  let mChannel = message.mentioned.channels.first()
+  if(mChannel) {
+  argsresult = args.slice(1).join(" ")
+  mChannel.send(argsresult)
+  } else {
+  
+  argsresult = args.join(" ")
+  argsresult.channel.send(argsresult)
+  }
+}
+
+module.exports.config = {
+  name: "a",
+  description: "Make an announcement to the server",
+  usage:"/a",
+  accessableby:"Developer, Bot Devs, Admins",
+  aliases:["acc", "announcement"]
 }
