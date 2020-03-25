5385d1a3cba99b643b629d624779b4d648c92752
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2020-03-25 11:26:17 -0400

    Added 1 Navy unit

--- coders/sea.js
+++ coders/sea.js
@@ -7,17 +7,17 @@ let db = require("./databasel.json");
 client.setMaxListeners(0)
 //_____________Dont touch Zone end
  
- const e20 = new Discord.RichEmbed()
+ const e0 = new Discord.RichEmbed()
 	.setColor('#1500f7')
-	.setTitle('Combat Engineer')
+	.setTitle('Gunboat')
 	.setURL('')
-	.attachFiles(['./resources/cengineer.jpg'])
+	.attachFiles(['./resources/gunboat.jpg'])
 	.setAuthor('LemonsHQ', 'https://cdn.discordapp.com/avatars/242687584373964801/4b20eb44bfff0b11f7447ed582fedbb2.png?size=128')
-	.setDescription(" - Can build units and some T2 buildings \n- Can attack land, air, and subs \n- Can repair units \n- Can build Sub-turrets \n- Can only have 12 built at once")
+	.setDescription(" - Fast \n - Can attack ground")
 	.addBlankField()
-	.addField("Stats", " Price $7400 \n Health 1500 \n Attack Range 200 \n Move Speed 1.20 \n Build Speed 33.3s \n Income generation +$1 \n InIClass combatEngineerX")
+	.addField("Stats", " Price $300 \n Health 170 \n Attack Range 120 \n Move Speed 1.5 \n Build Speed 33.3s \n InIClass c_gunboat")
 	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
-	.setImage('attachment://cengineer.jpg')
+	.setImage('attachment://gunboat.jpg')
 	.setTimestamp()
 	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
 
@@ -26,7 +26,7 @@ client.on('message', message => {
 
 //tank
 if (message.content.toLowerCase() === db.A) {
-	message.channel.send(e1);
+	message.channel.send(e0);
 }
   
 });
