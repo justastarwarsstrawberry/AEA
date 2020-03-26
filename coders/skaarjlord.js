3fddb0aebe37a31412543f05c2ee4c5340528169
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2020-03-26 17:22:52 -0400

    Added avatars

--- coders/skaarjlord.js
+++ coders/skaarjlord.js
@@ -16,7 +16,7 @@ const config = require("./prefix.json");
          const a1 = new Discord.RichEmbed()
          	.setColor('#1500f7')
 	        .setTitle('Announcement')
-         	.setAuthor('SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128')
+         	.setAuthor('SkaarjLord', message.author.displayAvatarURL())
          	.setDescription(taco)
          	.addBlankField()
          	.setURL('')
