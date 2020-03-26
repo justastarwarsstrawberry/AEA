6c9483aa194ff5ae0f6c7d0a3327b0beafeea832
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2020-03-26 12:54:23 -0400

    Update skaarjlord.js

--- coders/skaarjlord.js
+++ coders/skaarjlord.js
@@ -4,23 +4,34 @@ exports.code = function(){
 const { Client, Attachment } = require('discord.js');
 client.setMaxListeners(0)
 //_____________Dont touch Zone end
+ 
+ 
+ const taco = message.content
+         const a1 = new Discord.RichEmbed()
+         	.setColor('#1500f7')
+	         .setTitle('Announcement')
+         	.setAuthor('SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128')
+         	.setDescription(taco)
+         	.addBlankField()
+          .setURL('')
+	         .setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
+         	.setTimestamp()
+         	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
+ 
+ 
+ 
+ 
  client.on("message", message => {
     if(message.author.bot) return;
+  
+  
+  
+ let channels = message.mentions.channels; 
+ if(message.content.toLowerCase().startsWith('a')){
+     if(!channels) return;
+    message.content.send(a1);
 
-    let messageArray = message.content.split(" ");
-    let command = messageArray[0];
-    let args = messageArray.slice(1);
-
-    if(message.channel.type === "dm") return;
-
-    if(!message.content.startsWith('+')) return;
-
-    if(command === '+ann') {
-        let channel = message.mentions.channel();
-        let announcement = args.slice(1).join(" ");
-
-        channel.send(announcement);
-    }
+ }
   });
 }
 
