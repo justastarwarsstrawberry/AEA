c2322de3acfb6015abe92122520a2f18e7f4e3b4
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2020-03-26 13:01:04 -0400

    Debugging code take 2

--- coders/skaarjlord.js
+++ coders/skaarjlord.js
@@ -29,7 +29,7 @@ client.setMaxListeners(0)
  let channels = message.mentions.channels; 
  if(message.content.toLowerCase().startsWith('a')){
      if(!channels) return;
-    message.channels.send(a1);
+    message.channel.send(a1);
 
  }
   });
