650e3210ac17a569fa80ada7bb7e43aacfae731c
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2020-03-27 08:01:06 -0400

    Adding imports

--- coders/imports.js
+++ coders/imports.js
@@ -1,1 +1,42 @@
+exports.code = function(){
+const { Client, Attachment } = require('discord.js');
+client.setMaxListeners(0)
 
+
+const Sea = require('./sea.js');
+//const Func = require('./skaarjlord.js');
+const Anubis = require('./anubis.js');
+const Darkest = require('./land.js');
+const N = require('./Nword.js');
+const Timer = require('./timer.js');
+const Music = require('/index.js');
+  
+  
+  if(Timer && Client){
+   		 Timer.code();
+    		console.log('Timer loaded');
+  }
+
+  if(Anubis && Client){
+   		 Anubis.code();
+    		console.log('Anubis loaded');
+  }
+
+  if(Darkest && Client){
+   		Darkest.code();
+    		console.log('Land loaded');
+  }
+  if(Sea && Client){
+   		Sea.code();
+    		console.log('Sea loaded');
+  }
+  if(Music && Client){
+		Music.code();
+		console.log('Music loaded');
+  }
+  //if(Func && Client){
+  //   		Func.code();
+  //   		console.log('Skaarjlord loaded');
+  //}
+  
+}
