67ec8468894556388b9b50a0df3d390f1694600e
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2020-03-27 11:22:22 -0400

    import 1%

---
+++ imports/bot.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const config = require('./config.json');
+const client = new Discord.Client();
+client.config = config;
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+fs.readdir('./events/', (err, files) => {
+	if (err) return console.error(err);
+	files.forEach(file => {
+		const event = require(`./events/${file}`);
+		const eventName = file.split('.')[0];
+		client.on(eventName, event.bind(null, client));
+	});
+});
+
+client.login(config.token);
