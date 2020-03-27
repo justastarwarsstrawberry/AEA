d3c0060fb193a2cf76ac1405fa780fb0cdc197cc
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2020-03-27 08:15:03 -0400

    Removing comments

--- coders/imports.js
+++ coders/imports.js
@@ -1,18 +1,7 @@
 exports.code = function(){
 const { Client, Attachment } = require('discord.js');
 client.setMaxListeners(0)
---[
-. means the current path level (so if you're on index.aspx and you reference ./style.css then the latter would have to be in the same folder as the former)
-.. means one path level up (so if you're on /somefolder/index.aspx and you reference ../style.css then the latter would have to be in the parent folder of someFolder)
-/ means the root level (so /style.css is the same as http://www.mysite.com/style.css)
-~ in ASP.NET means the server-side application root (so ~/index.aspx would be translated to the URL of the index.aspx file that's in the application's root)	
- .    = this directory
- ../  = the parent directory
- ~/   = the user's home directory or the application's, in ASP
- /    = the root directory
- ../../ = the parent's parent directory 	
-	
-	]--
+
 
 const Sea = require('./sea.js');
 //const Func = require('./skaarjlord.js');
