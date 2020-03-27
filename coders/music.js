3fa759beb2e81d3b98fa44c346f7631049ef0e3b
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2020-03-27 09:20:19 -0400

    Adding commands

--- coders/music.js
+++ coders/music.js
@@ -1,1 +1,34 @@
+exports.code = function(){
 
+client.on("message", (message, user) => {
+  if (message.author.bot) return; // Good practice to do this.
+
+  // I set the Client to this just for ease.
+  // You'll probably have access to it another
+  // way, but this still works.
+  const client = message.client;
+
+  // Get the command from the message.
+  const command = message.substring(musicbot.botPrefix.length).split(/[ \n]/)[0].trim();
+
+  // Get the suffix, the String after the command.
+  const suffix = message.substring(musicbot.botPrefix.length + command.length).trim();
+
+  // Set the prefix to "!". This is a horrible way to set
+  // one, but it will do for now.
+  let prefix = "/"
+
+  // Now we check if the message starts with the prefix,
+  // and asks for the PLAY command.
+  if (message.content.startsWith(prefix) && command == "play") {
+    // Now we pass the Message Object (msg) and
+    // the suffix. It will then proceed as it would
+    // with the bot normally.
+    client.music.bot.playFunction(message, suffix);
+  }
+  
+  
+  
+});
+  
+}
