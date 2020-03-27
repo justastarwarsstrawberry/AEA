98bb25d4c45a6c1b57282d5130fe0cbf3160d5b6
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2020-03-27 08:10:04 -0400

    Updating imports

--- coders/imports.js
+++ coders/imports.js
@@ -1,7 +1,14 @@
 exports.code = function(){
 const { Client, Attachment } = require('discord.js');
 client.setMaxListeners(0)
-
+--[
+. means the current path level (so if you're on index.aspx and you reference ./style.css then the latter would have to be in the same folder as the former)
+.. means one path level up (so if you're on /somefolder/index.aspx and you reference ../style.css then the latter would have to be in the parent folder of someFolder)
+/ means the root level (so /style.css is the same as http://www.mysite.com/style.css)
+~ in ASP.NET means the server-side application root (so ~/index.aspx would be translated to the URL of the index.aspx file that's in the application's root)	
+	
+	
+	]--
 
 const Sea = require('./sea.js');
 //const Func = require('./skaarjlord.js');
@@ -9,7 +16,7 @@ const Anubis = require('./anubis.js');
 const Darkest = require('./land.js');
 const N = require('./Nword.js');
 const Timer = require('./timer.js');
-const Music = require('/index.js');
+const Music = require('~/index.js');
   
   
   if(Timer && Client){
