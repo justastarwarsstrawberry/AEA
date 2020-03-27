5bd15caa8404d926e4cc46e27c9a69cf3378ae40
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2020-03-27 09:45:35 -0400

    Removed old outdated code

--- coders/music.js
+++ coders/music.js
@@ -1,9 +1,7 @@
 exports.code = function(){
-client.music = require("discord.js-musicbot-addon");
 const Discord = require("discord.js");
 const { prefix, token } = require("../config.json");
 const ytdl = require("ytdl-core");
-
 const client = new Discord.Client();
 
 const queue = new Map();
