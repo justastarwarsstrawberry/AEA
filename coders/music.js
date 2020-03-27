2cda05a2e2e2f8511bffe2d0a89c4efb1b3218e0
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2020-03-27 09:43:19 -0400

    Update crash fix

--- coders/music.js
+++ coders/music.js
@@ -1,7 +1,7 @@
 exports.code = function(){
 client.music = require("discord.js-musicbot-addon");
 const Discord = require("discord.js");
-const { prefix, token } = require("./config.json");
+const { prefix, token } = require("../config.json");
 const ytdl = require("ytdl-core");
 
 const client = new Discord.Client();
