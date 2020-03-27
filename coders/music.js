d5f2314985e9d0181ab064a4063c3f419752004e
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2020-03-27 11:03:38 -0400

    Updating code

--- coders/music.js
+++ coders/music.js
@@ -1,40 +1,54 @@
 exports.code = function(){
-// your index.js should look similar to this:
-const { CommandoClient } = require('discord.js-commando');
-const { Structures } = require('discord.js');
-const path = require('path');
-const { prefix } = require('./prefix.json');
-// It's vital this is before the initiation of the client
-Structures.extend('Guild', Guild => {
-  class MusicGuild extends Guild {
-    constructor(client, data) {
-      super(client, data);
-      this.musicData = {
-        queue: [],
-        isPlaying: false,
-        volume: 1,
-        songDispatcher: null
-      };
-    }
-  }
-  return MusicGuild;
-});
-const client = new CommandoClient({
-  commandPrefix: prefix,
-  owner: 'your-discord-user-id',
-  unknownCommandResponse: false
-});
+const { Client, Attachment } = require('discord.js');
+const ytdl = require("ytdl-core");
+const PREFIX = "/";
+var server = {},
+client.once('message', message => {
+  
+  let args = message.content.substring(PREFIX.length).split(" ");
+  
+        switch(args[0]){
+          case 'play':
+            
+            function play(connection,message){
+              var server = servers [message.guild.id]
+              server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}))
+              
+              server.queue.shift();
+              
+              server.dispatcher.on("end", function(){
+                if(server.queue[0]){
+                  play(connection, message)
+                }else {
+                  connection.disconnect();
+                }
+              });
+            }
+            
+            
+            if(!args[1]){
+              message.channel.send("provide a link")
+              return;
+            }
+            
+            
+            if(!message.member.voiceChannel){
+              message.channel.send("You must be in a Voice chat")
+              return;
+            }
+            if(!servers[message.guild.id])servers[mesage.guild.id] = {
+              queue:[]
+            }
+            
+            var server = server[message.guild.id];
+            
+            server.queue.push(args[1]);
+            
+            if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection){
+              play(connection, message);
+            })
+            break;
+        }
 
-client.registry
-  .registerDefaultTypes()
-  .registerGroups([
-    ['music', 'Music Command Group']
-  ])
-  .registerDefaultGroups()
-  .registerDefaultCommands()
-  .registerCommandsIn(path.join(__dirname, 'commands'));
-
-client.once('ready', () => {
-  console.log('Music ready!');
 });
 }
