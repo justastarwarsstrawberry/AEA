3a08cd744a200d64af35c9e5345a41159b9e400c
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-03-28 20:28:11 -0400

    Updating Betas

--- resources/music/music.js
+++ resources/music/music.js
@@ -1,6 +1,7 @@
 exports.code = function(){
+var one = process.env.BOT_TOKEN
 var exec = require('child_process').exec, child;
-child = exec('java -jar ./resources/music/Music.jar',
+child = exec('java -jar ./resources/music/Music.jar one',
   function (error, stdout, stderr){
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
