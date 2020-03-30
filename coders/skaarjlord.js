3c6486d80388c85e043c04b5b5f23cad84f99515
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-03-30 18:39:39 -0400

    Updating Reactions

--- coders/skaarjlord.js
+++ coders/skaarjlord.js
@@ -7,9 +7,55 @@ client.setMaxListeners(0)
 //_____________Dont touch Zone end
 const config = require("./prefix.json");
 
+//    Add emoji name
+var emojiname = ["",""];
+
+//    Add role name
+var rolename=["",""];
+
+
+client.on('message', msg => {
+
+if(msg.content.startsWith("/") && msg.content.endsWith("reaction"){
+  if(!msg.channel.guild) return;
+  for(let n in emojiname){
+  var emoji =[msg.guild.emojis.find(r => r.name == emojiname[n])];
+  for(let i in emoji){
+   msg.react(emoji[i]);
+  }
+ }
+}
+});
+
+
+
+client.on("messageReactionAdd",(reaction,user)=>{
+  if(!user) return;
+  if(user.bot)return;
+  if(!reaction.message.channel.guild) return;
+  for(let n in emojiname){
+  if(reaction.emoji.name == emojiname[n]){
+    let role = reaction.message.guild.roles.find(r => r.name == rolename[n]);          
+    reaction.message.guild.member(user).addRole(role).catch(console.error);
+  }
+}
+});
+
+
+client.on("messageReactionRemove",(reaction,user)=>{
+  if(!user) return;
+  if(user.bot)return;
+  if(!reaction.message.channel.guild) return;
+  for(let n in emojiname){
+  if(reaction.emoji.name == emojiname[n]){
+    let role = reaction.message.guild.roles.find(r => r.name == rolename[n]);   
+    reaction.message.guild.member(user).removeRole(role).catch(console.error);
+  }
+  }
+});
+
  
- 
- client.on("message", message => {
+client.on("message", message => {
 let guild = client.guilds.get('606586202942079017');
 let member = guild.member(message.author);
 let nickname = member ? member.displayName : null;
