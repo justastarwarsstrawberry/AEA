43416a1bc4a2c62d7bdf57e29832282ffe147e89
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-03-29 21:03:33 -0400

    Updating libraries

--- resources/music/music.js
+++ resources/music/music.js
@@ -1,12 +1,12 @@
 exports.code = function(){
-var one = process.env.BOT_TOKEN
 var exec = require('child_process').exec, child;
-child = exec('java -jar ./resources/music/Music.jar one',
+child = exec('java -Dnogui=true -jar ./resources/music/Music.jar', (input) =>
   function (error, stdout, stderr){
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
     if(error !== null){
       console.log('exec error: ' + error);
     }
+
 });
 }
