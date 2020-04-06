258c98a99ea78fe3fa32227dd08ed29cd98d033c
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-04-06 00:05:58 -0400

    Updating nword database

--- coders/Nword.js
+++ coders/Nword.js
@@ -18,7 +18,7 @@ const nworde = new Discord.RichEmbed()
 	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
 
 client.on('message', (message, user) => {
-    if (message.content.toLowerCase().includes(db.A) || message.content.toLowerCase().includes(db.B) || message.content.toLowerCase().includes(db.B) || message.content.toLowerCase().includes(db.C) || message.content.toLowerCase().includes(db.D) || message.content.toLowerCase().includes(db.E) || message.content.toLowerCase().includes(db.F) || message.content.toLowerCase().includes(db.G) || message.content.toLowerCase().includes(db.H) ) {
+    if (message.content.toLowerCase().includes(db.A) || message.content.toLowerCase().includes(db.B) || message.content.toLowerCase().includes(db.H) || message.content.toLowerCase().includes(db.C) || message.content.toLowerCase().includes(db.D) || message.content.toLowerCase().includes(db.E) || message.content.toLowerCase().includes(db.F) || message.content.toLowerCase().includes(db.G) || message.content.toLowerCase().includes(db.H) || message.content.toLowerCase().includes(db.I) ) {
 		if(message.author.bot) return;
 		message.delete(); 
                message.channel.send(nworde);
