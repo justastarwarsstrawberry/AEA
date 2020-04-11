0839b020365e8d3ff963dabea9014672c352b9fe
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-04-11 14:30:03 -0400

    v16

--- coders/land.js
+++ coders/land.js
@@ -277,6 +277,19 @@ const e20 = new Discord.RichEmbed()
 	.setImage('attachment://cengineer.jpg')
 	.setTimestamp()
 	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
+const e21 = new Discord.RichEmbed()
+	.setColor('#1500f7')
+	.setTitle('Siege Tank')
+	.setURL('')
+	.attachFiles(['./resources/siegetank.jpg'])
+	.setAuthor('Lemons#5444', 'https://imgur.com/1ir1AqD.png')
+	.setDescription(" - Can build units and some T2 buildings \n- Can attack land, air, and subs \n- Can repair units \n- Can build Sub-turrets \n- Can only have 12 built at once")
+	.addBlankField()
+	.addField("Stats", " Price $7400 \n Health 1500 \n Attack Range 200 \n Move Speed 1.20 \n Build Speed 33.3s \n Income generation +$1 \n InIClass combatEngineerX")
+	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
+	.setImage('attachment://seigetank.jpg')
+	.setTimestamp()
+	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
 
 client.on('message', message => {
 	
