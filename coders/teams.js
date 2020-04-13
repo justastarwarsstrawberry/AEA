f719d89faf264769b6983177effbfced480dbc6b
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-04-13 14:03:27 -0400

    v56
    Adding more variables

--- coders/teams.js
+++ coders/teams.js
@@ -8,10 +8,20 @@ exports.code = function(){
       
     client.on("message", message => {
         client.guilds.get('606586202942079017').channels.get('651205041356537891').fetchMessage('699060018007375935');
-        let Team1 = ["bob","bob","bob","bob","bob"];
-        let Team2 = ["bob","bob","bob","bob","bob"];
+       
+        let Team1 = [T1S,T1A,T1B,T1C,T1D];
+        let Team2 = [T2S,T2A,T2B,T2C,T2D];
         if (message.content.toLowerCase().startsWith('/team')){
-
+            var T1S = "";
+            var T1A = "";
+            var T1B = "";
+            var T1C = "";
+            var T1D = "";
+            var T2S = "";
+            var T2A = "";
+            var T2B = "";
+            var T2C = "";
+            var T2D = "";
             var counts = 0;
             var counta = 0;
             var countb = 0;
@@ -24,104 +34,69 @@ exports.code = function(){
             let dclass = message.guild.roles.find(role => role.name === "D - Class");
             const nick = message.author.displayName;
                 if (message.member.roles.some(role => role.name === 'S - Class')){
-                    if(counts == 1){
+                    if(counts == 2){
 
-                    Team2[0] = message.member.user.tag
+                    T2S = message.member.user.tag
                     counts--
-                    fs.writeFile('./teams/team2.txt', Team2, function (err) {
-                        if (err) return console.log(err);
-                        console.log('Hello World > team2.txt');
-                      });
+
                     }else {
 
-                    Team1[0] = message.member.user.tag
+                    T1S = message.member.user.tag
                     counts++
-                    fs.writeFile('./teams/team1.txt', Team1, function (err) {
-                        if (err) return console.log(err);
-                        console.log('Hello World > team1.txt');
-                    });
+
                     }
 
                 }
                 if(message.member.roles.some(role => role.name === 'A - Class')){
-                    if(counta == 1){
-                    Team2[1] = message.member.user.tag
+                    if(counta == 2){
+                    T2A = message.member.user.tag
                     counta--
-                    fs.writeFile('./teams/team2.txt', Team2, function (err) {
-                        if (err) return console.log(err);
-                        console.log('Hello World > team2.txt');
-                    });
+
                     }else{
-                    Team1[1] = message.member.user.tag
+                    T1A = message.member.user.tag
                     counta++
-                    fs.writeFile('./teams/team1.txt', Team1, function (err) {
-                        if (err) return console.log(err);
-                        console.log('Hello World > team1.txt');
-                    });
+
                     }
 
 
                 }
                 if(message.member.roles.some(role => role.name === 'B - Class')){
-                    if(countb == 1){
-                    Team2[2] = message.member.user.tag
+                    if(countb == 2){
+                    T2B = message.member.user.tag
                     countb--
 
                     }else {
-                    Team1[2] = message.member.user.tag
+                    T1B = message.member.user.tag
                     countb++
 
                     }
                 }
                 if(message.member.roles.some(role => role.name === 'C - Class')){
-                    if(countc == 1){
-                    Team2[3] = message.member.user.tag
+                    if(countc == 2){
+                    T2C = message.member.user.tag
                     countc--
-                    fs.writeFile('./teams/team2.txt', Team2, function (err) {
-                        if (err) return console.log(err);
-                        console.log('Hello World > team2.txt');
-                    });
+
                     }else{
-                    Team1[3] = message.member.user.tag
+                    T1C = message.member.user.tag
                     countc++
-                    fs.writeFile('./teams/team1.txt', Team2, function (err) {
-                        if (err) return console.log(err);
-                        console.log('Hello World > team1.txt');
-                    });
+
                     }
                 }
                 if(message.member.roles.some(role => role.name === 'D - Class')){
-                    if(countd == 1){
+                    if(countd == 2){
                     Team2[4] = message.member.user.tag
                     countd--
-                    fs.writeFile('./teams/team2.txt', Team2, function (err) {
-                        if (err) return console.log(err);
-                        console.log('Hello World > team2.txt');
-                    });
+
                     }else{
                     Team1[4] = message.member.user.tag
                     countd++
-                    fs.writeFile('./teams/team1.txt', Team2, function (err) {
-                        if (err) return console.log(err);
-                        console.log('Hello World > team1.txt');
-                    });
+
                     }
                 }
             }
-                if(message.content.toLowerCase().startsWith('/rteams')){
-                    
-                    fs.unlink('./teams/team1.txt', function (err) {
-                    if (err) throw err;
-                    console.log('team1 reset');
-                    }); 
-                    fs.unlink('./teams/team2.txt', function (err) {
-                        if (err) throw err;
-                        console.log('Team2 reset');
-                    }); 
-                }
             if(message.content.toLowerCase().startsWith('/lteams')){
-                message.channel.send('Team 1' + Team1)
-                message.channel.send('Team 2' + Team2)
+                message.channel.send('Team 1: ' + Team1)
+                message.channel.send('Team 2: ' + Team2)
             }
 
         
