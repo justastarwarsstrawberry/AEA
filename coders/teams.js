30c0f9ee4b3758e798b861a52ad1bb7c15ffb250
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-04-12 21:22:15 -0400

    v27
    Getting there

--- coders/teams.js
+++ coders/teams.js
@@ -15,58 +15,59 @@ exports.code = function(){
             let dclass = message.guild.roles.find(role => role.name === "D - Class");
 
                 if (message.member.roles.some(role => role.name === 'S - Class')){
-                    s = member.user.id();
+                    s = message.member.user.id();
                     for(var i = 0; i < array.length; ++i){
                         if(array[i] == s)
                          count++;
                       }
                 }
                 if(message.member.roles.some(role => role.name === 'A - Class')){
-                    a = member.user.id();
+                    a = message.member.user.id();
                     for(var i = 0; i < array.length; ++i){
                         if(array[i] == a)
                          count++;
                       }
                 }
                 if(message.member.roles.some(role => role.name === 'B - Class')){
-                    b = member.user.id();
+                    b = message.member.user.id();
                     for(var i = 0; i < array.length; ++i){
                         if(array[i] == b)
                          count++;
                       }
                 }
                 if(message.member.roles.some(role => role.name === 'C - Class')){
-                    c = member.user.id();
+                    c = message.member.user.id();
                     for(var i = 0; i < array.length; ++i){
                         if(array[i] == c)
                          count++;
                       }
                 }
                 if(message.member.roles.some(role => role.name === 'D - Class')){
-                    d = member.user.id();
+                    d = message.member.user.id();
                     for(var i = 0; i < array.length; ++i){
                         if(array[i] == d)
                          count++;
                       }
                 }
-        message.channel.send('Team 1 | Team 2 | Team 3')
-        if(s === 2 || s === 3){
-            message.channel.send(s)
-        }
-        if(a === 2 || a === 3){
-            message.channel.send(a)
-        }
-        if(b === 2 || b === 3){
-            message.channel.send(b)
-        }
-        if(c === 2 || c === 3){
-            message.channel.send(c)
-        }
-        if(d === 2 || d === 3){
-            message.channel.send(d)
-        }
-
+             }
+    if(message.content.toLowerCase().startsWith('/teams')){}
+    message.channel.send('Team 1 | Team 2 | Team 3')
+    if(s === 2 || s === 3){
+        message.channel.send(s)
     }
+    if(a === 2 || a === 3){
+        message.channel.send(a)
+    }
+    if(b === 2 || b === 3){
+        message.channel.send(b)
+    }
+    if(c === 2 || c === 3){
+        message.channel.send(c)
+    }
+    if(d === 2 || d === 3){
+        message.channel.send(d)
+    }
+}
     });
 
 }
