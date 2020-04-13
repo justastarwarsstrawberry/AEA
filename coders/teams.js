23ca81178b9bdf02b45b9f8e1b69521ba2b4cb55
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-04-12 21:01:26 -0400

    v23
    Fixing some things so the bot doesnt break

--- coders/teams.js
+++ coders/teams.js
@@ -6,79 +6,70 @@ exports.code = function(){
       
     client.on("message", message => {
         client.guilds.get('606586202942079017').channels.get('651205041356537891').fetchMessage('699060018007375935');
-    });
-    client.on('messageReactionAdd', async(reaction, user)=> {
-        let message = reaction.message, emoji = reaction.emoji;
-        let teams = [s,a,b,c,d]
+        if (message.content.toLowerCase.startsWith('/team')){
+        message.guild.fetchMember(user.id).then(member => {
+            var count = 0;
+            let sclass = message.guild.roles.find(role => role.name === "S - Class");
+            let aclass = message.guild.roles.find(role => role.name === "A - Class");
+            let bclass = message.guild.roles.find(role => role.name === "B - Class");
+            let cclass = message.guild.roles.find(role => role.name === "C - Class");
+            let dclass = message.guild.roles.find(role => role.name === "D - Class");
 
-        message.reaction.add('🥊');
-        if (emoji.id == '🥊') {
-            // We don't have the member, but only the user...
-            // Thanks to the previous part, we know how to fetch it
-            message.guild.fetchMember(user.id).then(member => {
-                var count = 0;
-                let sclass = message.guild.roles.find(role => role.name === "S - Class");
-                let aclass = message.guild.roles.find(role => role.name === "A - Class");
-                let bclass = message.guild.roles.find(role => role.name === "B - Class");
-                let cclass = message.guild.roles.find(role => role.name === "C - Class");
-                let dclass = message.guild.roles.find(role => role.name === "D - Class");
+                if (member.roles.some(role => role.name === 'S - Class')){
+                    s = member.user.id();
+                    for(var i = 0; i < array.length; ++i){
+                        if(array[i] == s)
+                         count++;
+                      }
+                }
+                if(member.roles.some(role => role.name === 'A - Class')){
+                    a = member.user.id();
+                    for(var i = 0; i < array.length; ++i){
+                        if(array[i] == a)
+                         count++;
+                      }
+                }
+                if(member.roles.some(role => role.name === 'B - Class')){
+                    b = member.user.id();
+                    for(var i = 0; i < array.length; ++i){
+                        if(array[i] == b)
+                         count++;
+                      }
+                }
+                if(member.roles.some(role => role.name === 'C - Class')){
+                    c = member.user.id();
+                    for(var i = 0; i < array.length; ++i){
+                        if(array[i] == c)
+                         count++;
+                      }
+                }
+                if(member.roles.some(role => role.name === 'D - Class')){
+                    d = member.user.id();
+                    for(var i = 0; i < array.length; ++i){
+                        if(array[i] == d)
+                         count++;
+                      }
+                }
+        });
+        message.channel.send('Team 1 | Team 2 | Team 3')
+        if(s === 2 || s === 3){
+            message.channel.send(s)
+        }
+        if(a === 2 || a === 3){
+            message.channel.send(a)
+        }
+        if(b === 2 || b === 3){
+            message.channel.send(b)
+        }
+        if(c === 2 || c === 3){
+            message.channel.send(c)
+        }
+        if(d === 2 || d === 3){
+            message.channel.send(d)
+        }
 
-                    if (member.roles.some(role => role.name === 'S - Class')){
-                        s = member.user.id();
-                        for(var i = 0; i < array.length; ++i){
-                            if(array[i] == s)
-                             count++;
-                          }
-                    }
-                    if(member.roles.some(role => role.name === 'A - Class')){
-                        a = member.user.id();
-                        for(var i = 0; i < array.length; ++i){
-                            if(array[i] == a)
-                             count++;
-                          }
-                    }
-                    if(member.roles.some(role => role.name === 'B - Class')){
-                        b = member.user.id();
-                        for(var i = 0; i < array.length; ++i){
-                            if(array[i] == b)
-                             count++;
-                          }
-                    }
-                    if(member.roles.some(role => role.name === 'C - Class')){
-                        c = member.user.id();
-                        for(var i = 0; i < array.length; ++i){
-                            if(array[i] == c)
-                             count++;
-                          }
-                    }
-                    if(member.roles.some(role => role.name === 'D - Class')){
-                        d = member.user.id();
-                        for(var i = 0; i < array.length; ++i){
-                            if(array[i] == d)
-                             count++;
-                          }
-                    }
-            });
-            message.channel.send('Team 1 | Team 2 | Team 3')
-            if(s === 2 || s === 3){
-                message.channel.send(s)
-            }
-            if(a === 2 || a === 3){
-                message.channel.send(a)
-            }
-            if(b === 2 || b === 3){
-                message.channel.send(b)
-            }
-            if(c === 2 || c === 3){
-                message.channel.send(c)
-            }
-            if(d === 2 || d === 3){
-                message.channel.send(d)
-            }
- 
     }
     });
-
 
 }
 //________________Dont touch Zone end
