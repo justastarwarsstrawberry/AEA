7b808819e26a4c54307f6e4ae7514ac153d1d979
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-04-13 13:22:31 -0400

    v50
    Working on file write system

--- coders/teams.js
+++ coders/teams.js
@@ -1,6 +1,8 @@
 //_____________Dont touch Zone start
 exports.code = function(){
     const { Client, Attachment } = require('discord.js');
+    const fs = require("fs");
+    const config = require("./database.json");
     client.setMaxListeners(0)
     
       
@@ -23,51 +25,105 @@ exports.code = function(){
             const nick = message.author.displayName;
                 if (message.member.roles.some(role => role.name === 'S - Class')){
                     if(counts == 1){
+
                     Team2[0] = message.member.user.tag
                     counts--
+                    fs.writeFile('./teams/team2.txt', Team2, function (err) {
+                        if (err) return console.log(err);
+                        console.log('Hello World > team2.txt');
+                      });
                     }else {
+
                     Team1[0] = message.member.user.tag
                     counts++
+                    fs.writeFile('./teams/team1.txt', Team1, function (err) {
+                        if (err) return console.log(err);
+                        console.log('Hello World > team1.txt');
+                    });
                     }
+
                 }
                 if(message.member.roles.some(role => role.name === 'A - Class')){
                     if(counta == 1){
                     Team2[1] = message.member.user.tag
                     counta--
+                    fs.writeFile('./teams/team2.txt', Team2, function (err) {
+                        if (err) return console.log(err);
+                        console.log('Hello World > team2.txt');
+                    });
                     }else{
                     Team1[1] = message.member.user.tag
                     counta++
+                    fs.writeFile('./teams/team1.txt', Team1, function (err) {
+                        if (err) return console.log(err);
+                        console.log('Hello World > team1.txt');
+                    });
                     }
+
+
                 }
                 if(message.member.roles.some(role => role.name === 'B - Class')){
                     if(countb == 1){
                     Team2[2] = message.member.user.tag
                     countb--
+                    fs.writeFile('./teams/team2.txt', Team2, function (err) {
+                        if (err) return console.log(err);
+                        console.log('Hello World > team2.txt');
+                    });
                     }else {
                     Team1[2] = message.member.user.tag
                     countb++
+                    fs.writeFile('./teams/team1.txt', Team2, function (err) {
+                        if (err) return console.log(err);
+                        console.log('Hello World > team1.txt');
+                    });
                     }
                 }
                 if(message.member.roles.some(role => role.name === 'C - Class')){
                     if(countc == 1){
                     Team2[3] = message.member.user.tag
                     countc--
+                    fs.writeFile('./teams/team2.txt', Team2, function (err) {
+                        if (err) return console.log(err);
+                        console.log('Hello World > team2.txt');
+                    });
                     }else{
                     Team1[3] = message.member.user.tag
                     countc++
+                    fs.writeFile('./teams/team1.txt', Team2, function (err) {
+                        if (err) return console.log(err);
+                        console.log('Hello World > team1.txt');
+                    });
                     }
                 }
                 if(message.member.roles.some(role => role.name === 'D - Class')){
                     if(countd == 1){
                     Team2[4] = message.member.user.tag
                     countd--
+                    fs.writeFile('./teams/team2.txt', Team2, function (err) {
+                        if (err) return console.log(err);
+                        console.log('Hello World > team2.txt');
+                    });
                     }else{
                     Team1[4] = message.member.user.tag
                     countd++
+                    fs.writeFile('./teams/team1.txt', Team2, function (err) {
+                        if (err) return console.log(err);
+                        console.log('Hello World > team1.txt');
+                    });
                     }
                 }
 
-                message.channel.send('Team1:' + Team1)
+                if(message.content.toLowerCase.startsWith('/teams')){
+                    fs.unlink('./teams/team1.txt', function (err) {
+                    if (err) throw err;
+                    console.log('team1 reset');
+                    }); 
+                    fs.unlink('./teams/team2.txt', function (err) {
+                        if (err) throw err;
+                        console.log('Team2 reset');
+                    }); 
+                }
             }
 
         
