bd4ee2beab1f62a5f508b8f0271d1109a92acc24
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-04-16 18:35:02 -0400

    Added Jquery
    For the tournament info

---
+++ coders/JQuery.js
@@ -0,0 +1,25 @@
+exports.code = function(){
+const { Client, Attachment } = require('discord.js');
+const fs = require("fs");
+client.setMaxListeners(0)
+var $ = require('jQuery');
+
+
+
+client.on('message', message => {
+var $ = require('jQuery');
+if(message.content.startsWith("/tournament")){
+$('.demo_iframe').challonge('wgamhen5', {subdomain: '', theme: '1', multiplier: '1.0', match_width_multiplier: '1.0', show_final_results: '0', show_standings: '0'}).appendTo("body");
+message.reply($("body").html());
+}
+
+
+
+});
+  
+  
+  
+  
+//________________Dont touch Zone start
+}
+//________________Dont touch Zone end
