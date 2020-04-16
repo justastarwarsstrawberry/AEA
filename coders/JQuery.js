2cd394cea8e8d71259ebd7c54ebdd70a3ba50914
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-04-16 19:54:45 -0400

    Trying another thing

--- coders/JQuery.js
+++ coders/JQuery.js
@@ -2,7 +2,13 @@ exports.code = function(){
 const { Client, Attachment } = require('discord.js');
 const fs = require("fs");
 const { JSDOM } = require( "jsdom" );
-const dom = new JSDOM(`<iframe src="https://challonge.com/wgamhen5/module" width="100%" height="500" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>`);
+const dom = new JSDOM(`<iframe src="https://challonge.com/wgamhen5/module" width="100%" height="500" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>`, {
+    url: "https://challonge.com/wgamhen5/module",
+    referrer: "https://challonge.com/wgamhen5/module",
+    contentType: "text/html",
+    includeNodeLocations: true,
+    storageQuota: 10000000
+  });
 const $ = require( "jquery" );
 client.setMaxListeners(0)
 
