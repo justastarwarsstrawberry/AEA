1e9aa70a0d9f94483d333f983f9c8f7dd089f10f
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-04-16 19:19:47 -0400

    Adding windows for Jquery
    Jsdom

--- coders/JQuery.js
+++ coders/JQuery.js
@@ -1,13 +1,14 @@
 exports.code = function(){
 const { Client, Attachment } = require('discord.js');
 const fs = require("fs");
+const $ = require( "jquery" )( window );
 client.setMaxListeners(0)
 
 
 
 
 client.on('message', message => {
-var $ = require('jquery');
+
 if(message.content.startsWith("/test")){
 $('.demo_iframe').challonge('wgamhen5', {subdomain: '', theme: '1', multiplier: '1.0', match_width_multiplier: '1.0', show_final_results: '0', show_standings: '0'}).appendTo("body");
 message.reply($("body").html());
