29d5638772b046231a9866c378346c6413912c7e
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-04-16 19:57:37 -0400

    Trying again

--- coders/JQuery.js
+++ coders/JQuery.js
@@ -2,7 +2,7 @@ exports.code = function(){
 const { Client, Attachment } = require('discord.js');
 const fs = require("fs");
 const { JSDOM } = require( "jsdom" );
-const dom = new JSDOM(`<iframe src="https://challonge.com/wgamhen5/module" width="100%" height="500" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>`, {
+const dom = new JSDOM(`<iframe src="https://challonge.com/wgamhen5/module" width="100%" height="500" frameborder="0" scrolling="auto" allowtransparency="true">type here</iframe>`, {
     url: "https://challonge.com/wgamhen5/module",
     referrer: "https://challonge.com/wgamhen5/module",
     contentType: "text/html",
@@ -19,7 +19,7 @@ client.on('message', message => {
 
 if(message.content.startsWith("/test")){
 
-message.reply(dom.window.document.querySelector("iframe").textContent);
+message.reply(dom.window.document.querySelector("iframe"));
 }
 
 
