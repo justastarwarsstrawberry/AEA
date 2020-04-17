da1cc1a24f3e992f8dc8065001398cc2037da8a2
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-04-16 20:27:45 -0400

    Fixing code

--- coders/JQuery.js
+++ coders/JQuery.js
@@ -1,6 +1,6 @@
 exports.code = function(){
 const { Client, Attachment } = require('discord.js');
-const challonge = require('challonge');
+
 const { JSDOM } = require( "jsdom" );
 const { window } = new JSDOM( "" );
 const $ = require( "jquery" )( window );
@@ -10,7 +10,7 @@ client.setMaxListeners(0)
 
 
 client.on('message', message => {
-
+const challonge = require('challonge');
 if(message.content.startsWith("/test")){
 
 message.reply($('.demo_iframe').challonge('wgamhen5', {subdomain: '', theme: '1', multiplier: '1.0', match_width_multiplier: '1.0', show_final_results: '0', show_standings: '0'}));
