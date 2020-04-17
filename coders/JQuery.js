ed716b94459ae925cf5c482b727c52463506747c
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-04-16 20:36:21 -0400

    Trying again

--- coders/JQuery.js
+++ coders/JQuery.js
@@ -12,8 +12,8 @@ client.setMaxListeners(0)
 client.on('message', message => {
 
 if(message.content.startsWith("/test")){
-var challonge = require('challonge')
-message.reply(challonge('wgamhen5', {subdomain: '', theme: '1', multiplier: '1.0', match_width_multiplier: '1.0', show_final_results: '0', show_standings: '0'}));
+
+message.reply($('https://challonge.com/wgamhen5', {subdomain: '', theme: '1', multiplier: '1.0', match_width_multiplier: '1.0', show_final_results: '0', show_standings: '0'}));
 
 }
 
