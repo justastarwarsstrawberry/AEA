61c327082600b299d3ae9bcc5815525229f12e61
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-04-16 21:34:01 -0400

    Updating code

--- coders/JQuery.js
+++ coders/JQuery.js
@@ -15,15 +15,15 @@ client.on('message', message => {
        const taco = message.content
              const a1 = new Discord.RichEmbed()
                 .setColor('#1500f7')
-                .setAuthor(nickname, avatar)
+               // .setAuthor(nickname, avatar)
                 .setTitle(taco)
-                .setImage(image)
+               // .setImage(image)
                // .setImage('attachment://tank.png')
-                .addBlankField()
+               // .addBlankField()
                 .setURL('https://challonge.com/wgamhen5.svg')
                 .setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
-                .setTimestamp()
-                .setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
+                // .setTimestamp()
+               // .setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
    
 
 if(message.content.toLowerCase().startsWith('>')){
@@ -31,16 +31,14 @@ if(message.content.toLowerCase().startsWith('>')){
     
     if(message.member.roles.some(role => role.name === 'Bot Developer')){
         if(message.author.bot) return;  
-        message.channel.send(a1)
-        /*
+        
        let channel = message.guild.channels.find(channel => channel.name === "bracket")
        if(!channel){
-        message.channel.send(a1)
-           message.reply('The "annoucements" channel must exist')
+           message.reply('The target channel must exist')
        }else {
            message.channel.send(a1)
        }
-       */
+       
        
     }
 }
