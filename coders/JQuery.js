1505020186799ddb31f1a804702cd03e145b10b5
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-04-20 13:50:12 -0400

    Updating Bot Announcements
    Changed the bot devs announcement command

--- coders/JQuery.js
+++ coders/JQuery.js
@@ -10,20 +10,20 @@ client.on('message', message => {
     let guild = client.guilds.get('606586202942079017');
     let member = guild.member(message.author);
     let nickname = member ? member.displayName : null;
-    let avatar = member ? member.avatarURL : null;
+    let avatar = message.member.avatarURL;
     let image = message.Attachment
        const taco = message.content
              const a1 = new Discord.RichEmbed()
                 .setColor('#1500f7')
-               // .setAuthor(nickname, avatar)
-               .setTitle('Tournament Bracket')
+               .setTitle('Update')
+               .setAuthor(nickname, avatar)
                 .setDescription(taco)
                // .setImage(image)
                // .setImage('attachment://tank.png')
                // .addBlankField()
-                .setURL('https://challonge.com/wgamhen5.svg')
+                .setURL('https://github.com/LemonsHQ/AEA')
                 .setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
-                // .setTimestamp()
+                .setTimestamp()
                // .setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
    
 
