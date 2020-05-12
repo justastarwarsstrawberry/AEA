eebdd58e1f63176facc945051f854cbb6c6ac4ae
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2020-05-11 17:57:08 -0700

    adding user mentions

--- code/other/muteco.js
+++ code/other/muteco.js
@@ -12,20 +12,35 @@ client.on("message", message => {
 
 
 if(message.content.toLowerCase().startsWith('/mute')){
-    let member = message.mentions.users;
-    if (!member) return message.reply('You have not mentioned a user!')
+
+
+    function getUserFromMention(mention) {
+        if (!mention) return;
+    
+        if (mention.startsWith('<@') && mention.endsWith('>')) {
+            mention = mention.slice(2, -1);
+    
+            if (mention.startsWith('!')) {
+                mention = mention.slice(1);
+            }
+    
+            return client.users.cache.get(mention);
+        }
+    }
+    if (args[0]) {
+	const user = getUserFromMention(args[0]);
+    if (!user) return message.reply('You have not mentioned a user!')
     let muteRole = message.guild.roles.find('name', 'Muted');
     if (!muteRole) return message.reply('You dont have a "Muted" role!')
     let params = message.content.split(" ").slice(1);
     let time = params[1];
     if(!time) return message.reply('There is no specified time!')
-
-    member.addRole('709296636609953792');
-    message.channel.send(`${member.user.tag} has been muted for ${ms(ms(time), {long:true})}...`);
-
+    user.addRole(muteRole);
+    message.channel.send(`${user.user.tag} has been muted for ${ms(ms(time), {long:true})}...`);
+    }
 setTimeout(function() {
-    member.removeRole('709296636609953792');
-    message.channel.send(`${member.user.tag} is unmuted, mute time was ${ms(ms(time), {long:true})}...`);
+    user.removeRole(muteRole);
+    message.channel.send(`${user.user.tag} is unmuted, mute time was ${ms(ms(time), {long:true})}...`);
 }, ms(time));
     }
 });
