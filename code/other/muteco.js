4713b5b14808ced4f81076e9d8fb7d4f845aac75
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2020-05-11 17:28:09 -0700

    Updating methods
    trying a new way to add roles

--- code/other/muteco.js
+++ code/other/muteco.js
@@ -12,17 +12,18 @@ client.on("message", message => {
 
 
 if(message.content.toLowerCase().startsWith('/mute')){
-    let member = message.mentions.users;
-    if (!member) return message.reply('You have not mentioned a user!')
+    let mentioned = message.mentions.users;
+    if (!mentioned) return message.reply('You have not mentioned a user!')
     let muteRole = message.guild.roles.find('name', 'Muted');
     if (!muteRole) return message.reply('You dont have a "Muted" role!')
     let params = message.content.split(" ").slice(1);
     let time = params[1];
     if(!time) return message.reply('There is no specified time!')
-
+    let id = mentioned.id
+	message.guild.fetchMember(id).then(member => {
     member.addRole(muteRole.id);
     message.channel.send(`${member.user.tag} has been muted for ${ms(ms(time), {long:true})}...`);
-    
+    });
 setTimeout(function() {
     member.removeRole(muteRole.id);
     message.channel.send(`${member.user.tag} is unmuted, mute time was ${ms(ms(time), {long:true})}...`);
