2cfb225051fdd21d9f5a94bd3546596e703ca597
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2020-05-17 19:46:58 -0400

    Applying a quick hotfix
    Added user avatar

--- code/other/skaarjlord.js
+++ code/other/skaarjlord.js
@@ -33,11 +33,12 @@ client.on("message", message => {
 let guild = client.guilds.get('606586202942079017');
 let member = guild.member(message.author);
 let nickname = member ? member.displayName : null;
+let userava = message.author;
    const taco = message.content
          const a1 = new Discord.RichEmbed()
          	.setColor('#1500f7')
 	        .setTitle('New Suggestion!')
-         	.setAuthor(nickname, 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128')
+         	.setAuthor(nickname, userava.avatarURL)
 			 .setDescription(taco)
 			.addField('Vote Options:','✅ or ❌')
          	.addBlankField()
