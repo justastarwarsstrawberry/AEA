c8f6f15711ca1d25a69b0f6c91231efc9f755215
Strawberryslol <68839402+justastarwarsstrawberry@users.noreply.github.com>
2020-07-28 00:05:25 +0800

    Working on arty turret

--- code/units/BUILDING.js
+++ code/units/BUILDING.js
@@ -84,6 +84,25 @@ exports.code = function(){
 
 
 
+         //Artturret
+     const a3 = new Discord.RichEmbed()
+	.setColor('#1500f7')
+	.setTitle('artelleryturret')
+	.setURL('')
+	.attachFiles(['./resources/turrett1.png'])
+	.setAuthor('justasweetstrawberry[🇵🇭]#5774', 'https://imgur.com/TlX2rnJ.png')
+	.setDescription(" -Can only target ground units\n-Multiple upgrades available")
+	.addBlankField()
+	.addField("Stats", "Price $2000 \n Health 1100 \n Attack Range 250 \n Move Speed 3.3s \n Build Speed 32s \n t2gunLand")
+	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
+	.setImage('')
+	.setTimestamp()
+	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
+
+
+
+
+
 
 	//Dont touch zone start
     client.on('message', message => {
