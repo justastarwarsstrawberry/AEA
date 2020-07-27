b9d4b795e1c8d6a450de6db6f4e028584a40b633
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2020-07-27 02:13:36 -0400

    Making template for strawberry

--- code/units/BUILDING.js
+++ code/units/BUILDING.js
@@ -1,6 +1,8 @@
+//Dont touch zone start
 exports.code = function(){
     const { Client, Attachment } = require('discord.js');
     client.setMaxListeners(0)
+//Dont touch zone end
 
     const tlist = new Discord.RichEmbed()
 	.setColor('#1500f7')
@@ -12,28 +14,35 @@ exports.code = function(){
 	.addField("Turrets", "T1Turret \n T2Turret \n T3Turret \n ArtilleryTurret")
     .setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
 	.setTimestamp()
-	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
-	
+    .setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
+    
+
+
+	//Dont touch zone start
     client.on('message', message => {
+    //Dont touch zone end
+
+
+
+
+
+
+
+
+
         if (message.content.toLowerCase() === 'turrets' || message.content.toLowerCase() === 'turret') {
             message.channel.send(tlist);
+
+
+
+
+
+
+
+
+
+	//Dont touch zone start
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     });
-
-
-
 }
+    //Dont touch zone end
