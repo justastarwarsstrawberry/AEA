67dbe5c47b2617947a65045fbf5327be5035f75a
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2020-07-26 21:16:21 -0400

    Fixing some things again

--- code/other/reactionroles.js
+++ code/other/reactionroles.js
@@ -13,12 +13,12 @@ exports.code = function(){
 
         
         if(message.content.toLowerCase().startsWith('}') && client.users.get("242687584373964801")){
-            let mes1 = message.content
+            const mes1 = message.content
             let chan = message.guild.channels.find(channel => channel.name === "bot-testing")
             let option1 = system.createOption("aea:737107148533792829", "697809380137107478");
             let option2 = system.createOption("hosts:737107269300256857", "711766395817558067" );
             let option3 = system.createOption("nukedrone:737107875117400114", "694316498553929778");
-            let resend = system.createMessage(mes1, chan, 9999, null, option1, option2, option3);
+            const resend = system.createMessage(mes1, chan, 9999, null, option1, option2, option3);
 
             chan.send(resend)
             system.init();
