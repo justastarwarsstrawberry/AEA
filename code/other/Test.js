efda79f6f3149d1c88da692a5ffc3e3eddb1f3be
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2020-07-27 12:59:53 -0400

    Fixing strawberry's code

--- code/other/Test.js
+++ code/other/Test.js
@@ -4,24 +4,18 @@ exports.code = function(){
       
     client.on("message", message => {
 
-    if (message.content.toLowerCase() === 'rage') {
+
+   if(message.content.toLowerCase.includes('rage') &&  client.users.get("543626502215041025") ){
         if(message.author.bot) return;
-        message.channel.send();
+        message.reply('rage? I heard galaxy like to rage like a bitch?');
     }
-    client.users.get("543626502215041025")
-    message.content.includes('rage? I heard galaxy like to rage like a bitch?') &&
-  
-    }
+    
 
 
-    if (message.content.toLowerCase() === 'host') {
+    if (message.content.toLowerCase() === 'host' && client.users.get("428543881978707969") ) {
         if(message.author.bot) return;
-        message.channel.send();
+        message.channel.send('host? Can I host?');
     }
-
-    client.users.get("428543881978707969")
-    message.content.includes('host I can host? ') &&
-
 
 
     });
