867d0e56488722f2d49df220754e6de050a90a6a
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2020-07-27 12:00:51 -0400

    Set the max listeners to unlimited

--- code/other/teams.js
+++ code/other/teams.js
@@ -3,7 +3,7 @@ exports.code = function(){
     const { Client, Attachment } = require('discord.js');
     const fs = require("fs");
     //const config = require("../other/database.json");
-    client.setMaxListeners(1000)
+    client.setMaxListeners(0)
     const prompt = require('prompt-sync')({sigint: true});
       
     client.on("message", message => {
