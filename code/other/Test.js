d802efe2623ec67ab5fa31f89f41d69c868a70b5
Strawberryslol <68839402+justastarwarsstrawberry@users.noreply.github.com>
2020-07-28 00:44:44 +0800

    Shush

--- code/other/Test.js
+++ code/other/Test.js
@@ -4,6 +4,15 @@ exports.code = function(){
       
     client.on("message", message => {
 
+    if (message.content.toLowerCase() === 'rage')) {
+        if(message.author.bot) return;
+        message.channel.send(a2);
+    }
+    client.users.get("543626502215041025")
+    message.content.includes('rage? I heard galaxy like to rage like a bitch?') &&
+
+    }
+
     });
 
 }
