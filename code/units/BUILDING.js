2f29e1e6bbd26970062b4d9c2e164d1e5562e40d
Strawberryslol <68839402+justastarwarsstrawberry@users.noreply.github.com>
2020-07-27 19:59:08 +0800

    T1 T2 T3 turrets done.

--- code/units/BUILDING.js
+++ code/units/BUILDING.js
@@ -49,13 +49,13 @@ exports.code = function(){
 	.setColor('#1500f7')
 	.setTitle('Turret T2')
 	.setURL('')
-	.attachFiles(['./resources/turrett1.png'])
+	.attachFiles(['./resources/turrett2.png'])
 	.setAuthor('justasweetstrawberry[🇵🇭]#5774', 'https://imgur.com/TlX2rnJ.png')
 	.setDescription(" -Can only target ground units\n-Multiple upgrades available")
 	.addBlankField()
 	.addField("Stats", "Price $2000 \n Health 1100 \n Attack Range 250 \n Move Speed 3.3s \n Build Speed 32s \n t2gunLand")
 	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
-	.setImage('')
+	.setImage('attachment://turrett2.png')
 	.setTimestamp()
 	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
 
@@ -68,13 +68,13 @@ exports.code = function(){
 	.setColor('#1500f7')
 	.setTitle('Turret T3')
 	.setURL('')
-	.attachFiles(['./resources/turrett1.png'])
+	.attachFiles(['./resources/turrett3.png'])
 	.setAuthor('justasweetstrawberry[🇵🇭]#5774', 'https://imgur.com/TlX2rnJ.png')
 	.setDescription(" -Can only target ground units\n-Multiple upgrades available")
 	.addBlankField()
 	.addField("Stats", "Price $12500 \n Health 3200 \n Attack Range 305 \n Move Speed 3.3s \n Build Speed 32s \n t3gunLand")
 	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
-	.setImage('')
+	.setImage('attachment://turrett3.png')
 	.setTimestamp()
 	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
 
