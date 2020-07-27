57c2e01e84331882ff74bd96f38b167d22408e49
Strawberryslol <68839402+justastarwarsstrawberry@users.noreply.github.com>
2020-07-27 18:00:24 +0800

    T2 turret Des done.

--- code/units/BUILDING.js
+++ code/units/BUILDING.js
@@ -44,6 +44,20 @@ exports.code = function(){
 
 
 
+    const a2 = new Discord.RichEmbed()
+	.setColor('#1500f7')
+	.setTitle('Turret T2')
+	.setURL('')
+	.attachFiles([''])
+	.setAuthor('justasweetstrawberry[🇵🇭]#5774', 'https://imgur.com/TlX2rnJ.png')
+	.setDescription(" -Can only target ground units\n-Multiple upgrades available")
+	.addBlankField()
+	.addField("Stats", "Price $2000 \n Health 1100 \n Attack Range 250 \n Move Speed 6s \n Build Speed 32s")
+	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
+	.setImage('')
+	.setTimestamp()
+	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
+
 
 
 
@@ -61,6 +75,10 @@ exports.code = function(){
         message.channel.send(a1);
     }
 
+
+    if (message.content.toLowerCase() === 't2turret') {
+        message.channel.send(a2)
+    }
 
 
     if (message.content.toLowerCase() === 'turrets' || message.content.toLowerCase() === 'turret') {
