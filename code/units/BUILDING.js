9eb03f7cb2b197917ade596879c30ac447e884a7
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2020-07-28 22:27:08 -0400

    Added arty turret

--- code/units/BUILDING.js
+++ code/units/BUILDING.js
@@ -95,7 +95,7 @@ exports.code = function(){
 	.addBlankField()
 	.addField("Stats", "Price $2500 \n Health 1100 \n Attack Range 360 \n Move Speed 0s \n Build Speed 32s \n InIclass t1arty")
 	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
-	.setImage('')
+	.setImage('attachment://artilleryturret.png')
 	.setTimestamp()
 	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
 
@@ -110,13 +110,13 @@ exports.code = function(){
 	.setColor('#1500f7')
 	.setTitle('Turret flame')
 	.setURL('')
-	.attachFiles(['./resources/turrett1.png'])
+	.attachFiles(['./resources/turrett3.png'])
 	.setAuthor('justasweetstrawberry[🇵🇭]#5774', 'https://imgur.com/TlX2rnJ.png')
 	.setDescription(" -Can only target ground units\n-Multiple upgrades available")
 	.addBlankField()
 	.addField("Stats", "Price $2000 \n Health 1100 \n Attack Range 250 \n Move Speed 3.3s \n Build Speed 32s \n t2gunLand")
 	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
-	.setImage('')
+	.setImage('attachment://turrett3.png')
 	.setTimestamp()
 	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
 
