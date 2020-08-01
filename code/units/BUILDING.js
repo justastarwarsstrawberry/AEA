b53802d103dd2001a4da5538787d29f324f6d9d7
Strawberryslol <68839402+justastarwarsstrawberry@users.noreply.github.com>
2020-08-01 23:16:30 +0800

    Adding some stuff

--- code/units/BUILDING.js
+++ code/units/BUILDING.js
@@ -20,7 +20,7 @@ exports.code = function(){
 	.setAuthor('Lemons#5444', 'https://i.imgur.com/9wxw0Sg.png')
 	.setDescription("A list of all turrets to be typed and displayed")
 	.addBlankField()
-	.addField("Turrets", "T1Turret \n T2Turret \n T3Turret \n ArtilleryTurret \n flame turret")
+	.addField("Turrets", "T1Turret \n T2Turret \n T3Turret \n ArtilleryTurret \n flame turret \n rocket turret")
     .setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
 	.setTimestamp()
     .setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
@@ -120,6 +120,26 @@ exports.code = function(){
 	.setTimestamp()
 	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
 
+
+
+
+
+
+
+         //rocket turret
+    const a5 = new Discord.RichEmbed()
+	.setColor('#1500f7')
+	.setTitle('Turret flame')
+	.setURL('')
+	.attachFiles(['./resources/rocketturret.png'])
+	.setAuthor('justasweetstrawberry[🇵🇭]#5774', 'https://imgur.com/TlX2rnJ.png')
+	.setDescription(" -can attack ground and air. \n long range. .")
+	.addBlankField()
+	.addField("Stats", "Price $1,800 \n Health 500 \n Attack Range 180 \n Move Speed 0s \n Build Speed 32s \n InIclass t1flameLand")
+	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
+	.setImage('attachment://rocketturret.png')
+	.setTimestamp()
+	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
 
 
 
