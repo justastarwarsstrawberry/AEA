46c6e8fac7cbeb8e94a7fa4a0e7e12597417f607
Strawberryslol <68839402+justastarwarsstrawberry@users.noreply.github.com>
2020-08-02 16:26:36 +0800

    Spelling fix

--- code/units/BUILDING.js
+++ code/units/BUILDING.js
@@ -166,12 +166,12 @@ exports.code = function(){
 
 
 
-           //light canon
+           //light cannon
     const a8 = new Discord.RichEmbed()
 	.setColor('#1500f7')
 	.setTitle('canon')
 	.setURL('')
-	.attachFiles(['./resources/lightcanon.png'])
+	.attachFiles(['./resources/lightcannon.png'])
 	.setAuthor('justasweetstrawberry[🇵🇭]#5774', 'https://imgur.com/TlX2rnJ.png')
 	.setDescription(" -can attack ground and air. \n long range. .")
 	.addBlankField()
@@ -188,10 +188,10 @@ exports.code = function(){
 
 
 
-          //heavy canon
+          //heavy cannon
     const a9 = new Discord.RichEmbed()
 	.setColor('#1500f7')
-	.setTitle('heavy canon')
+	.setTitle('heavy cannon')
 	.setURL('')
 	.attachFiles(['./resources/heavycanon.png'])
 	.setAuthor('justasweetstrawberry[🇵🇭]#5774', 'https://imgur.com/TlX2rnJ.png')
@@ -199,7 +199,7 @@ exports.code = function(){
 	.addBlankField()
 	.addField("Stats", "Price $2,400 \n Health 900 \n Attack Range 300  \n Build Speed 32s \n InIclass rocket_turret")
 	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
-	.setImage('attachment://heavycanon.png')
+	.setImage('attachment://heavycannon.png')
 	.setTimestamp()
 	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
 
